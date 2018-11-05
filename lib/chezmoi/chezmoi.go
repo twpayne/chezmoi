bb5fdffa28bcfd24ffa09e99781080653e1a00c0
Tom Payne <twpayne@gmail.com>
2018-11-05 13:55:44 +0100

    Add initial library

--- lib/chezmoi/chezmoi.go
+++ lib/chezmoi/chezmoi.go
@@ -2,65 +2,273 @@ package chezmoi
 
 import (
 	"bytes"
+	"io/ioutil"
 	"os"
+	"path/filepath"
+	"reflect"
 	"regexp"
+	"strings"
 	"text/template"
 
+	"github.com/absfs/afero"
 	"github.com/pkg/errors"
 )
 
 var (
-	nameRegexp        = regexp.MustCompile(`\A(?P<private>private_)?(?P<executable>executable_)?(?P<dot>dot_)?(?P<name>.*?)(?P<template>\.tmpl)?\z`)
-	nameSubexpIndexes = makeSubexpIndexes(nameRegexp)
+	dirNameRegexp         = regexp.MustCompile(`\A(?P<private>private_)?(?P<dot>dot_)?(?P<name>.*)\z`)
+	dirNameSubexpIndexes  = makeSubexpIndexes(dirNameRegexp)
+	fileNameRegexp        = regexp.MustCompile(`\A(?P<private>private_)?(?P<executable>executable_)?(?P<dot>dot_)?(?P<name>.*?)(?P<template>\.tmpl)?\z`)
+	fileNameSubexpIndexes = makeSubexpIndexes(fileNameRegexp)
 )
 
+// A FileState represents the target state of a file.
 type FileState struct {
 	Name     string
 	Mode     os.FileMode
 	Contents []byte
 }
 
-type State []*FileState
+// A DirState represents the target state of a directory.
+type DirState struct {
+	Name  string
+	Mode  os.FileMode
+	Dirs  map[string]*DirState
+	Files map[string]*FileState
+}
 
-func ParseFileState(filename string, contents []byte, data interface{}) (*FileState, error) {
-	m := nameRegexp.FindStringSubmatch(filename)
+// parseDirName parses a single directory name. It returns the target name,
+// mode, and any error.
+func parseDirName(dirName string) (string, os.FileMode, error) {
+	m := dirNameRegexp.FindStringSubmatch(dirName)
 	if m == nil {
-		return nil, errors.Errorf("invalid source name %q", filename)
+		return "", os.FileMode(0), errors.Errorf("invalid directory name: %s", dirName)
 	}
-	name := m[nameSubexpIndexes["name"]]
-	if m[nameSubexpIndexes["dot"]] != "" {
+	name := m[dirNameSubexpIndexes["name"]]
+	if m[dirNameSubexpIndexes["dot"]] != "" {
+		name = "." + name
+	}
+	mode := os.FileMode(0777)
+	if m[dirNameSubexpIndexes["private"]] != "" {
+		mode &= 0700
+	}
+	return name, mode, nil
+}
+
+// parseFileName parses a single file name. It returns the target name, mode,
+// whether the contents should be interpreted as a template, and any error.
+func parseFileName(fileName string) (string, os.FileMode, bool, error) {
+	m := fileNameRegexp.FindStringSubmatch(fileName)
+	if m == nil {
+		return "", os.FileMode(0), false, errors.Errorf("invalid file name: %s", fileName)
+	}
+	name := m[fileNameSubexpIndexes["name"]]
+	if m[fileNameSubexpIndexes["dot"]] != "" {
 		name = "." + name
 	}
 	mode := os.FileMode(0666)
-	if m[nameSubexpIndexes["executable"]] != "" {
+	if m[fileNameSubexpIndexes["executable"]] != "" {
 		mode |= 0111
 	}
-	if m[nameSubexpIndexes["private"]] != "" {
+	if m[fileNameSubexpIndexes["private"]] != "" {
 		mode &= 0700
 	}
-	if m[nameSubexpIndexes["template"]] != "" {
-		tmpl, err := template.New(filename).Parse(string(contents))
-		if err != nil {
-			return nil, errors.Wrap(err, filename)
-		}
-		output := &bytes.Buffer{}
-		if err := tmpl.Execute(output, data); err != nil {
-			return nil, errors.Wrap(err, filename)
-		}
-		contents = output.Bytes()
-	}
-	return &FileState{
-		Name:     name,
-		Mode:     mode,
-		Contents: contents,
-	}, nil
+	isTemplate := m[fileNameSubexpIndexes["template"]] != ""
+	return name, mode, isTemplate, nil
 }
 
-type byName State
+// parseDirNameComponents parses multiple directory name components. It returns
+// the target directory names, target modes, and any error.
+func parseDirNameComponents(components []string) ([]string, []os.FileMode, error) {
+	dirNames := []string{}
+	modes := []os.FileMode{}
+	for _, component := range components {
+		dirName, mode, err := parseDirName(component)
+		if err != nil {
+			return nil, nil, err
+		}
+		dirNames = append(dirNames, dirName)
+		modes = append(modes, mode)
+	}
+	return dirNames, modes, nil
+}
 
-func (bn byName) Len() int           { return len(bn) }
-func (bn byName) Less(i, j int) bool { return bn[i].Name < bn[j].Name }
-func (bn byName) Swap(i, j int)      { bn[i], bn[j] = bn[j], bn[i] }
+// parseFilePath parses a single file path. It returns the target directory
+// names, the target filename, the target mode, whether the contents should be
+// interpreted as a template, and any error.
+func parseFilePath(path string) ([]string, string, os.FileMode, bool, error) {
+	if path == "" {
+		return nil, "", os.FileMode(0), false, errors.New("empty path")
+	}
+	components := splitPathList(path)
+	dirNames, _, err := parseDirNameComponents(components[0 : len(components)-1])
+	if err != nil {
+		return nil, "", os.FileMode(0), false, err
+	}
+	fileName, mode, isTemplate, err := parseFileName(components[len(components)-1])
+	if err != nil {
+		return nil, "", os.FileMode(0), false, err
+	}
+	return dirNames, fileName, mode, isTemplate, nil
+}
+
+// newDirState returns a new directory state.
+func newDirState(name string, mode os.FileMode) *DirState {
+	return &DirState{
+		Name:  name,
+		Mode:  mode,
+		Dirs:  make(map[string]*DirState),
+		Files: make(map[string]*FileState),
+	}
+}
+
+// newRootDirState returns a new root directory state.
+func newRootDirState() *DirState {
+	return newDirState("", os.FileMode(0))
+}
+
+// isRoot returns whether ds refers to the root.
+func (ds *DirState) isRoot() bool {
+	return ds.Name == "" && ds.Mode == os.FileMode(0)
+}
+
+// Apply ensures that targetDir in fs matches ds.
+func (ds *DirState) Apply(fs afero.Fs, targetDir string) error {
+	if !ds.isRoot() {
+		if _, dirName := filepath.Split(targetDir); dirName != ds.Name {
+			return errors.Errorf("name mismatch: got %s, want %s", dirName, ds.Name)
+		}
+		fi, err := fs.Stat(targetDir)
+		switch {
+		case err == nil && fi.Mode().IsDir():
+			if fi.Mode()&os.ModePerm != ds.Mode {
+				if err := fs.Chmod(targetDir, ds.Mode); err != nil {
+					return err
+				}
+			}
+		case err == nil:
+			if err := fs.RemoveAll(targetDir); err != nil {
+				return err
+			}
+			fallthrough
+		case os.IsNotExist(err):
+			if err := fs.Mkdir(targetDir, ds.Mode); err != nil {
+				return err
+			}
+		default:
+			return err
+		}
+	}
+	for fileName, fileState := range ds.Files {
+		if err := fileState.Apply(fs, filepath.Join(targetDir, fileName)); err != nil {
+			return err
+		}
+	}
+	for dirName, dirState := range ds.Dirs {
+		if err := dirState.Apply(fs, filepath.Join(targetDir, dirName)); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// ReadSourceDirState walks fs from sourceDir creating a target directory
+// state. Any templates found are executed with data.
+func ReadSourceDirState(fs afero.Fs, sourceDir string, data interface{}) (*DirState, error) {
+	rootDS := newRootDirState()
+	if err := afero.Walk(fs, sourceDir, func(path string, fi os.FileInfo, err error) error {
+		if path == sourceDir {
+			return nil
+		}
+		relPath := strings.TrimPrefix(path, sourceDir)
+		switch {
+		case fi.Mode().IsRegular():
+			dirNames, fileName, mode, isTemplate, err := parseFilePath(relPath)
+			if err != nil {
+				return errors.Wrap(err, path)
+			}
+			ds := rootDS
+			for _, dirName := range dirNames {
+				ds = ds.Dirs[dirName]
+			}
+			r, err := fs.Open(path)
+			if err != nil {
+				return err
+			}
+			defer r.Close()
+			contents, err := ioutil.ReadAll(r)
+			if err != nil {
+				return errors.Wrap(err, path)
+			}
+			if isTemplate {
+				tmpl, err := template.New(path).Parse(string(contents))
+				if err != nil {
+					return errors.Wrap(err, path)
+				}
+				output := &bytes.Buffer{}
+				if err := tmpl.Execute(output, data); err != nil {
+					return errors.Wrap(err, path)
+				}
+				contents = output.Bytes()
+			}
+			ds.Files[fileName] = &FileState{
+				Name:     fileName,
+				Mode:     mode,
+				Contents: contents,
+			}
+		case fi.Mode().IsDir():
+			components := splitPathList(relPath)
+			dirNames, modes, err := parseDirNameComponents(components)
+			if err != nil {
+				return errors.Wrap(err, path)
+			}
+			ds := rootDS
+			for i := 0; i < len(dirNames)-1; i++ {
+				ds = ds.Dirs[dirNames[i]]
+			}
+			dirName := dirNames[len(dirNames)-1]
+			mode := modes[len(modes)-1]
+			ds.Dirs[dirName] = newDirState(dirName, mode)
+		default:
+			return errors.Errorf("unsupported file type: %s", path)
+		}
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+	return rootDS, nil
+}
+
+// Apply ensures that state of targetPath in fs matches fileState.
+func (fileState *FileState) Apply(fs afero.Fs, targetPath string) error {
+	if _, fileName := filepath.Split(targetPath); fileName != fileState.Name {
+		return errors.Errorf("name mismatch: got %s, want %s", targetPath, fileState.Name)
+	}
+	fi, err := fs.Stat(targetPath)
+	switch {
+	case err == nil && fi.Mode().IsRegular() && fi.Mode()&os.ModePerm == fileState.Mode:
+		f, err := fs.Open(targetPath)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		contents, err := ioutil.ReadAll(f)
+		if err != nil {
+			return errors.Wrap(err, targetPath)
+		}
+		if reflect.DeepEqual(contents, fileState.Contents) {
+			return nil
+		}
+	case err == nil:
+		if err := fs.RemoveAll(targetPath); err != nil {
+			return err
+		}
+	case os.IsNotExist(err):
+	default:
+		return err
+	}
+	// FIXME atomically replace
+	return afero.WriteFile(fs, targetPath, fileState.Contents, fileState.Mode)
+}
 
 func makeSubexpIndexes(re *regexp.Regexp) map[string]int {
 	result := make(map[string]int)
@@ -68,4 +276,12 @@ func makeSubexpIndexes(re *regexp.Regexp) map[string]int {
 		result[name] = index
 	}
 	return result
+}
+
+func splitPathList(path string) []string {
+	components := strings.Split(path, string(filepath.Separator))
+	if components[0] == "" {
+		return components[1:len(components)]
+	}
+	return components
 }
--- lib/chezmoi/chezmoi_test.go
+++ lib/chezmoi/chezmoi_test.go
@@ -1,97 +1,237 @@
 package chezmoi
 
 import (
+	"os"
+	"path/filepath"
 	"reflect"
 	"testing"
+
+	"github.com/absfs/afero"
+	"github.com/d4l3k/messagediff"
 )
 
-func TestParseFileState(t *testing.T) {
+func TestReadSourceDirState(t *testing.T) {
 	for _, tc := range []struct {
-		filename string
-		contents []byte
-		data     interface{}
-		wantErr  bool
-		want     *FileState
+		fs        map[string]string
+		sourceDir string
+		data      interface{}
+		want      *DirState
 	}{
 		{
-			filename: "foo",
-			want: &FileState{
-				Name: "foo",
-				Mode: 0666,
+			fs: map[string]string{
+				"/foo": "bar",
+			},
+			sourceDir: "/",
+			want: &DirState{
+				Name: "",
+				Mode: os.FileMode(0),
+				Dirs: map[string]*DirState{},
+				Files: map[string]*FileState{
+					"foo": &FileState{
+						Name:     "foo",
+						Mode:     os.FileMode(0666),
+						Contents: []byte("bar"),
+					},
+				},
 			},
 		},
 		{
-			filename: "foo",
-			contents: []byte("bar"),
-			want: &FileState{
-				Name:     "foo",
-				Mode:     0666,
-				Contents: []byte("bar"),
+			fs: map[string]string{
+				"/dot_foo": "bar",
+			},
+			sourceDir: "/",
+			want: &DirState{
+				Name: "",
+				Mode: os.FileMode(0),
+				Dirs: map[string]*DirState{},
+				Files: map[string]*FileState{
+					".foo": &FileState{
+						Name:     ".foo",
+						Mode:     os.FileMode(0666),
+						Contents: []byte("bar"),
+					},
+				},
 			},
 		},
 		{
-			filename: "foo.tmpl",
-			contents: []byte("{{23 -}} < {{- 45}}"),
-			want: &FileState{
-				Name:     "foo",
-				Mode:     0666,
-				Contents: []byte("23<45"),
+			fs: map[string]string{
+				"/private_foo": "bar",
+			},
+			sourceDir: "/",
+			want: &DirState{
+				Name: "",
+				Mode: os.FileMode(0),
+				Dirs: map[string]*DirState{},
+				Files: map[string]*FileState{
+					"foo": &FileState{
+						Name:     "foo",
+						Mode:     os.FileMode(0600),
+						Contents: []byte("bar"),
+					},
+				},
 			},
 		},
 		{
-			filename: "foo.tmpl",
-			contents: []byte("{{.User}}"),
-			data:     map[string]string{"User": "bar"},
-			want: &FileState{
-				Name:     "foo",
-				Mode:     0666,
-				Contents: []byte("bar"),
+			fs: map[string]string{
+				"/foo/bar": "baz",
+			},
+			sourceDir: "/",
+			want: &DirState{
+				Name: "",
+				Mode: os.FileMode(0),
+				Dirs: map[string]*DirState{
+					"foo": &DirState{
+						Name: "foo",
+						Mode: os.FileMode(0777),
+						Dirs: map[string]*DirState{},
+						Files: map[string]*FileState{
+							"bar": &FileState{
+								Name:     "bar",
+								Mode:     os.FileMode(0666),
+								Contents: []byte("baz"),
+							},
+						},
+					},
+				},
+				Files: map[string]*FileState{},
 			},
 		},
 		{
-			filename: "dot_bashrc",
-			want: &FileState{
-				Name: ".bashrc",
-				Mode: 0666,
+			fs: map[string]string{
+				"/private_dot_foo/bar": "baz",
+			},
+			sourceDir: "/",
+			want: &DirState{
+				Name: "",
+				Mode: os.FileMode(0),
+				Dirs: map[string]*DirState{
+					".foo": &DirState{
+						Name: ".foo",
+						Mode: os.FileMode(0700),
+						Dirs: map[string]*DirState{},
+						Files: map[string]*FileState{
+							"bar": &FileState{
+								Name:     "bar",
+								Mode:     os.FileMode(0666),
+								Contents: []byte("baz"),
+							},
+						},
+					},
+				},
+				Files: map[string]*FileState{},
 			},
 		},
 		{
-			filename: "private_dot_netrc",
-			want: &FileState{
-				Name: ".netrc",
-				Mode: 0600,
+			fs: map[string]string{
+				"/dot_gitconfig.tmpl": "[user]\n\temail = {{.Email}}\n",
 			},
-		},
-		{
-			filename: "executable_foo",
-			want: &FileState{
-				Name: "foo",
-				Mode: 0777,
+			sourceDir: "/",
+			data: map[string]string{
+				"Email": "user@example.com",
 			},
-		},
-		{
-			filename: "foo.tmpl",
-			want: &FileState{
-				Name: "foo",
-				Mode: 0666,
-			},
-		},
-		{
-			filename: "private_dot_bash_history.tmpl",
-			want: &FileState{
-				Name: ".bash_history",
-				Mode: 0600,
+			want: &DirState{
+				Name: "",
+				Mode: os.FileMode(0),
+				Dirs: map[string]*DirState{},
+				Files: map[string]*FileState{
+					".gitconfig": &FileState{
+						Name:     ".gitconfig",
+						Mode:     os.FileMode(0666),
+						Contents: []byte("[user]\n\temail = user@example.com\n"),
+					},
+				},
 			},
 		},
 	} {
-		if got, gotErr := ParseFileState(tc.filename, tc.contents, tc.data); (gotErr != nil) != tc.wantErr || !reflect.DeepEqual(got, tc.want) {
-			var wantErrStr string
-			if tc.wantErr {
-				wantErrStr = "!<nil>"
-			} else {
-				wantErrStr = "<nil>"
-			}
-			t.Errorf("ParseFileState(%q, %v, %v) == %v, %v, want %v, %v", tc.filename, tc.contents, tc.data, got, gotErr, tc.want, wantErrStr)
+		fs, err := makeMemMapFs(tc.fs)
+		if err != nil {
+			t.Errorf("makeMemMapFs(%v) == %v, %v, want !<nil>, <nil>", tc.fs, fs, err)
+			continue
+		}
+		if got, err := ReadSourceDirState(fs, tc.sourceDir, tc.data); err != nil || !reflect.DeepEqual(got, tc.want) {
+			diff, _ := messagediff.PrettyDiff(tc.want, got)
+			t.Errorf("ReadSourceDirState(makeMemMapFs(%v), %q, %v) == %+v, %v, want %+v, <nil>:\n%s", tc.fs, tc.sourceDir, tc.data, got, err, tc.want, diff)
 		}
 	}
+}
+
+func TestEndToEnd(t *testing.T) {
+	for i, tc := range []struct {
+		fsMap     map[string]string
+		sourceDir string
+		data      interface{}
+		targetDir string
+		wantFsMap map[string]string
+	}{
+		{
+			fsMap: map[string]string{
+				"/home/user/.bashrc":             "foo",
+				"/home/user/.chezmoi/dot_bashrc": "bar",
+			},
+			sourceDir: "/home/user/.chezmoi",
+			targetDir: "/home/user",
+			wantFsMap: map[string]string{
+				"/home/user/.bashrc":             "bar",
+				"/home/user/.chezmoi/dot_bashrc": "bar",
+			},
+		},
+	} {
+		fs, err := makeMemMapFs(tc.fsMap)
+		if err != nil {
+			t.Errorf("case %d: makeMemMapFs(%v) == %v, %v, want !<nil>, <nil>", i, tc.fsMap, fs, err)
+			continue
+		}
+		ds, err := ReadSourceDirState(fs, tc.sourceDir, tc.data)
+		if err != nil {
+			t.Errorf("case %d: ReadSourceDirState(makeMemMapFs(%v), %q, %v) == %v, %v, want !<nil>, <nil>", i, tc.fsMap, tc.sourceDir, tc.data, ds, err)
+			continue
+		}
+		if err := ds.Apply(fs, tc.targetDir); err != nil {
+			t.Errorf("case %d: %v.Apply(makeMemMapFs(%v), %q) == %v, want <nil>", i, ds, tc.fsMap, tc.targetDir, err)
+			continue
+		}
+		gotFsMap, err := makeMapFs(fs)
+		if err != nil {
+			t.Errorf("case %d: makeMapFs(%v) == %v, %v, want !<nil>, <nil>", i, fs, gotFsMap, err)
+			continue
+		}
+		if diff, equal := messagediff.PrettyDiff(tc.wantFsMap, gotFsMap); !equal {
+			t.Errorf("case %d:\n%s\n", i, diff)
+		}
+	}
+}
+
+func makeMemMapFs(fsMap map[string]string) (*afero.MemMapFs, error) {
+	//fs := afero.NewMemMapFs()
+	fs := &afero.MemMapFs{}
+	for path, contents := range fsMap {
+		if err := fs.MkdirAll(filepath.Dir(path), os.FileMode(0777)); err != nil {
+			return nil, err
+		}
+		if err := afero.WriteFile(fs, path, []byte(contents), os.FileMode(0666)); err != nil {
+			return nil, err
+		}
+	}
+	return fs, nil
+}
+
+func makeMapFs(fs afero.Fs) (map[string]string, error) {
+	mapFs := make(map[string]string)
+	if err := afero.Walk(fs, "/", func(path string, fi os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !fi.Mode().IsRegular() {
+			return nil
+		}
+		contents, err := afero.ReadFile(fs, path)
+		if err != nil {
+			return err
+		}
+		mapFs[path] = string(contents)
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+	return mapFs, nil
 }
