13936db750f00ab908c68e6586b0f2a4237fad8a
Tom Payne <twpayne@gmail.com>
2018-11-06 16:38:44 +0100

    Move helper functions into primary source file

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -3,10 +3,47 @@ package cmd
 import (
 	"log"
 	"os"
+	"path/filepath"
 	"syscall"
 
+	"github.com/absfs/afero"
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
+	"github.com/twpayne/chezmoi/lib/chezmoi"
 )
+
+func getSourceFileStates(targetState *chezmoi.RootState, targetFileNames []string) ([]*chezmoi.FileState, error) {
+	fileStates := []*chezmoi.FileState{}
+	for _, targetFileName := range targetFileNames {
+		fileState := targetState.FindSourceFile(targetFileName)
+		if fileState == nil {
+			return nil, errors.Errorf("%s: file not found", targetFileName)
+		}
+		fileStates = append(fileStates, fileState)
+	}
+	return fileStates, nil
+}
+
+func getSourceFileNames(targetState *chezmoi.RootState, targetFileNames []string) ([]string, error) {
+	fileStates, err := getSourceFileStates(targetState, targetFileNames)
+	if err != nil {
+		return nil, err
+	}
+	sourceFileNames := []string{}
+	for _, fileState := range fileStates {
+		sourceFileName := filepath.Join(sourceDir, fileState.SourceName)
+		sourceFileNames = append(sourceFileNames, sourceFileName)
+	}
+	return sourceFileNames, nil
+}
+
+func getTargetState(fs afero.Fs) (*chezmoi.RootState, error) {
+	targetState := chezmoi.NewRootState()
+	if err := targetState.Populate(fs, sourceDir, nil); err != nil {
+		return nil, err
+	}
+	return targetState, nil
+}
 
 func makeRun(runCommand func(*cobra.Command, []string) error) func(*cobra.Command, []string) {
 	return func(cmd *cobra.Command, args []string) {
--- cmd/root.go
+++ cmd/root.go
@@ -4,11 +4,8 @@ import (
 	"log"
 	"path/filepath"
 
-	"github.com/absfs/afero"
 	"github.com/mitchellh/go-homedir"
-	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
-	"github.com/twpayne/chezmoi/lib/chezmoi"
 )
 
 var (
@@ -38,37 +35,4 @@ func Execute() {
 	if err := rootCommand.Execute(); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func getSourceFileStates(targetState *chezmoi.RootState, targetFileNames []string) ([]*chezmoi.FileState, error) {
-	fileStates := []*chezmoi.FileState{}
-	for _, targetFileName := range targetFileNames {
-		fileState := targetState.FindSourceFile(targetFileName)
-		if fileState == nil {
-			return nil, errors.Errorf("%s: file not found", targetFileName)
-		}
-		fileStates = append(fileStates, fileState)
-	}
-	return fileStates, nil
-}
-
-func getSourceFileNames(targetState *chezmoi.RootState, targetFileNames []string) ([]string, error) {
-	fileStates, err := getSourceFileStates(targetState, targetFileNames)
-	if err != nil {
-		return nil, err
-	}
-	sourceFileNames := []string{}
-	for _, fileState := range fileStates {
-		sourceFileName := filepath.Join(sourceDir, fileState.SourceName)
-		sourceFileNames = append(sourceFileNames, sourceFileName)
-	}
-	return sourceFileNames, nil
-}
-
-func getTargetState(fs afero.Fs) (*chezmoi.RootState, error) {
-	targetState := chezmoi.NewRootState()
-	if err := targetState.Populate(fs, sourceDir, nil); err != nil {
-		return nil, err
-	}
-	return targetState, nil
 }
