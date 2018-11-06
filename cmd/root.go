261eaf47bd842a227fb246cae9986f5b71181aef
Tom Payne <twpayne@gmail.com>
2018-11-06 11:37:56 +0100

    Add source and target flags

--- cmd/root.go
+++ cmd/root.go
@@ -2,14 +2,34 @@ package cmd
 
 import (
 	"log"
+	"path/filepath"
 
+	"github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
+)
+
+var (
+	sourceDir string
+	targetDir string
 )
 
 var rootCommand = &cobra.Command{
 	Use:   "chezmoi",
 	Short: "chezmoi manages your home directory",
 	Run:   makeRun(runRootCommand),
+}
+
+func init() {
+	homeDir, err := homedir.Dir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	sourceDir = filepath.Join(homeDir, ".chezmoi")
+	targetDir = homeDir
+
+	persistentFlags := rootCommand.PersistentFlags()
+	persistentFlags.StringVar(&sourceDir, "source", sourceDir, "source directory")
+	persistentFlags.StringVar(&targetDir, "target", targetDir, "target directory")
 }
 
 func Execute() {
