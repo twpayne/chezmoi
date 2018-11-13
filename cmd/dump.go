5dc52c59c967dba3cf1b3971a7a1bbbc75cabe94
Tom Payne <twpayne@gmail.com>
2018-11-13 15:13:11 +0100

    Add args to dump command

--- cmd/dump.go
+++ cmd/dump.go
@@ -3,12 +3,12 @@ package cmd
 import (
 	"github.com/absfs/afero"
 	"github.com/davecgh/go-spew/spew"
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
 
 var dumpCommand = &cobra.Command{
 	Use:   "dump",
-	Args:  cobra.NoArgs, // FIXME should accept list of targets
 	Short: "Dump the target state",
 	RunE:  makeRunE(config.runDumpCommandE),
 }
@@ -22,6 +22,16 @@ func (c *Config) runDumpCommandE(fs afero.Fs, command *cobra.Command, args []str
 	if err != nil {
 		return err
 	}
-	spew.Dump(targetState)
+	if len(args) == 0 {
+		spew.Dump(targetState)
+	} else {
+		for _, arg := range args {
+			state := targetState.Get(arg)
+			if state == nil {
+				return errors.Errorf("%s: not found", arg)
+			}
+			spew.Dump(state)
+		}
+	}
 	return nil
 }
