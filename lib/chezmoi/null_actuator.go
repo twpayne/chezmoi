f0ff1859071cb4005584656b1dc0f3284af73ebc
Tom Payne <twpayne@gmail.com>
2018-11-20 19:50:29 +0100

    Document NullActuator

--- lib/chezmoi/null_actuator.go
+++ lib/chezmoi/null_actuator.go
@@ -5,11 +5,27 @@ import "os"
 // A NullActuator does nothing.
 type NullActuator struct{}
 
+// NewNullActuator returns a new NullActuator.
 func NewNullActuator() *NullActuator {
 	return &NullActuator{}
 }
 
-func (a *NullActuator) Chmod(string, os.FileMode) error                     { return nil }
-func (a *NullActuator) Mkdir(string, os.FileMode) error                     { return nil }
-func (a *NullActuator) RemoveAll(string) error                              { return nil }
-func (a *NullActuator) WriteFile(string, []byte, os.FileMode, []byte) error { return nil }
+// Chmod implements Actuator.Chmod.
+func (a *NullActuator) Chmod(string, os.FileMode) error {
+	return nil
+}
+
+// Mkdir implements Actuator.Mkdir.
+func (a *NullActuator) Mkdir(string, os.FileMode) error {
+	return nil
+}
+
+// RemoveAll implements Actuator.RemoveAll.
+func (a *NullActuator) RemoveAll(string) error {
+	return nil
+}
+
+// WriteFile implements Actuator.WriteFile.
+func (a *NullActuator) WriteFile(string, []byte, os.FileMode, []byte) error {
+	return nil
+}
