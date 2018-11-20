41db3b0cbaa8e94de01817b5f6c4f97bdb59cd56
Tom Payne <twpayne@gmail.com>
2018-11-20 19:50:20 +0100

    Document LoggingActuator

--- lib/chezmoi/logging_actuator.go
+++ lib/chezmoi/logging_actuator.go
@@ -9,16 +9,20 @@ import (
 	"github.com/sergi/go-diff/diffmatchpatch"
 )
 
+// A LoggingActuator wraps an Actuator and logs all of the actions it executes
+// and any errors.
 type LoggingActuator struct {
 	a Actuator
 }
 
+// NewLoggingActuator returns a new LoggingActuator.
 func NewLoggingActuator(a Actuator) *LoggingActuator {
 	return &LoggingActuator{
 		a: a,
 	}
 }
 
+// Chmod implements Actuator.Chmod.
 func (a *LoggingActuator) Chmod(name string, mode os.FileMode) error {
 	action := fmt.Sprintf("chmod %o %s", mode, name)
 	err := a.a.Chmod(name, mode)
@@ -30,6 +34,7 @@ func (a *LoggingActuator) Chmod(name string, mode os.FileMode) error {
 	return err
 }
 
+// Mkdir implements Actuator.Mkdir.
 func (a *LoggingActuator) Mkdir(name string, mode os.FileMode) error {
 	action := fmt.Sprintf("mkdir -m %o %s", mode, name)
 	err := a.a.Mkdir(name, mode)
@@ -41,6 +46,7 @@ func (a *LoggingActuator) Mkdir(name string, mode os.FileMode) error {
 	return err
 }
 
+// RemoveAll implements Actuator.RemoveAll.
 func (a *LoggingActuator) RemoveAll(name string) error {
 	action := fmt.Sprintf("rm -rf %s", name)
 	err := a.a.RemoveAll(name)
@@ -52,6 +58,7 @@ func (a *LoggingActuator) RemoveAll(name string) error {
 	return err
 }
 
+// WriteFile implements Actuator.WriteFile.
 func (a *LoggingActuator) WriteFile(name string, contents []byte, mode os.FileMode, currentContents []byte) error {
 	action := fmt.Sprintf("install -m %o /dev/null %s", mode, name)
 	err := a.a.WriteFile(name, contents, mode, currentContents)
