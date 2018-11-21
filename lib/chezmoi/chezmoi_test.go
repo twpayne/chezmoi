db7cd5a54e7dae47694156aa6203fbcb1ed6ba92
Tom Payne <twpayne@gmail.com>
2018-11-21 16:14:31 +0100

    Use subtests

--- lib/chezmoi/chezmoi_test.go
+++ lib/chezmoi/chezmoi_test.go
@@ -19,12 +19,14 @@ func TestDirName(t *testing.T) {
 		{dirName: "private_foo", name: "foo", mode: os.FileMode(0700)},
 		{dirName: "private_dot_foo", name: ".foo", mode: os.FileMode(0700)},
 	} {
-		if gotName, gotMode := parseDirName(tc.dirName); gotName != tc.name || gotMode != tc.mode {
-			t.Errorf("parseDirName(%q) == %q, %v, want %q, %v", tc.dirName, gotName, gotMode, tc.name, tc.mode)
-		}
-		if gotDirName := makeDirName(tc.name, tc.mode); gotDirName != tc.dirName {
-			t.Errorf("makeDirName(%q, %v) == %q, want %q", tc.name, tc.mode, gotDirName, tc.dirName)
-		}
+		t.Run(tc.dirName, func(t *testing.T) {
+			if gotName, gotMode := parseDirName(tc.dirName); gotName != tc.name || gotMode != tc.mode {
+				t.Errorf("parseDirName(%q) == %q, %v, want %q, %v", tc.dirName, gotName, gotMode, tc.name, tc.mode)
+			}
+			if gotDirName := makeDirName(tc.name, tc.mode); gotDirName != tc.dirName {
+				t.Errorf("makeDirName(%q, %v) == %q, want %q", tc.name, tc.mode, gotDirName, tc.dirName)
+			}
+		})
 	}
 }
 
@@ -45,23 +47,27 @@ func TestFileName(t *testing.T) {
 		{fileName: "foo.tmpl", name: "foo", mode: os.FileMode(0666), isEmpty: false, isTemplate: true},
 		{fileName: "private_executable_dot_foo.tmpl", name: ".foo", mode: os.FileMode(0700), isEmpty: false, isTemplate: true},
 	} {
-		if gotName, gotMode, gotIsEmpty, gotIsTemplate := parseFileName(tc.fileName); gotName != tc.name || gotMode != tc.mode || gotIsEmpty != tc.isEmpty || gotIsTemplate != tc.isTemplate {
-			t.Errorf("parseFileName(%q) == %q, %v, %v, %v want %q, %v, %v, %v", tc.fileName, gotName, gotMode, gotIsEmpty, gotIsTemplate, tc.name, tc.mode, tc.isEmpty, tc.isTemplate)
-		}
-		if gotFileName := makeFileName(tc.name, tc.mode, tc.isEmpty, tc.isTemplate); gotFileName != tc.fileName {
-			t.Errorf("makeFileName(%q, %v, %v, %v) == %q, want %q", tc.name, tc.mode, tc.isEmpty, tc.isTemplate, gotFileName, tc.fileName)
-		}
+		t.Run(tc.fileName, func(t *testing.T) {
+			if gotName, gotMode, gotIsEmpty, gotIsTemplate := parseFileName(tc.fileName); gotName != tc.name || gotMode != tc.mode || gotIsEmpty != tc.isEmpty || gotIsTemplate != tc.isTemplate {
+				t.Errorf("parseFileName(%q) == %q, %v, %v, %v want %q, %v, %v, %v", tc.fileName, gotName, gotMode, gotIsEmpty, gotIsTemplate, tc.name, tc.mode, tc.isEmpty, tc.isTemplate)
+			}
+			if gotFileName := makeFileName(tc.name, tc.mode, tc.isEmpty, tc.isTemplate); gotFileName != tc.fileName {
+				t.Errorf("makeFileName(%q, %v, %v, %v) == %q, want %q", tc.name, tc.mode, tc.isEmpty, tc.isTemplate, gotFileName, tc.fileName)
+			}
+		})
 	}
 }
 
 func TestRootStatePopulate(t *testing.T) {
 	for _, tc := range []struct {
+		name      string
 		fs        map[string]string
 		sourceDir string
 		data      map[string]interface{}
 		want      *RootState
 	}{
 		{
+			name: "simple_file",
 			fs: map[string]string{
 				"/foo": "bar",
 			},
@@ -81,6 +87,7 @@ func TestRootStatePopulate(t *testing.T) {
 			},
 		},
 		{
+			name: "dot_file",
 			fs: map[string]string{
 				"/dot_foo": "bar",
 			},
@@ -100,6 +107,7 @@ func TestRootStatePopulate(t *testing.T) {
 			},
 		},
 		{
+			name: "private_file",
 			fs: map[string]string{
 				"/private_foo": "bar",
 			},
@@ -119,6 +127,7 @@ func TestRootStatePopulate(t *testing.T) {
 			},
 		},
 		{
+			name: "file_in_subdir",
 			fs: map[string]string{
 				"/foo/bar": "baz",
 			},
@@ -145,6 +154,7 @@ func TestRootStatePopulate(t *testing.T) {
 			},
 		},
 		{
+			name: "file_in_private_dot_subdir",
 			fs: map[string]string{
 				"/private_dot_foo/bar": "baz",
 			},
@@ -171,6 +181,7 @@ func TestRootStatePopulate(t *testing.T) {
 			},
 		},
 		{
+			name: "template_dot_file",
 			fs: map[string]string{
 				"/dot_gitconfig.tmpl": "[user]\n\temail = {{.Email}}\n",
 			},
@@ -196,24 +207,25 @@ func TestRootStatePopulate(t *testing.T) {
 			},
 		},
 	} {
-		fs, err := absfstesting.MakeMemMapFs(tc.fs)
-		if err != nil {
-			t.Errorf("absfstesting.MakeMemMapFs(%v) == %v, %v, want !<nil>, <nil>", tc.fs, fs, err)
-			continue
-		}
-		rs := NewRootState("/", 0, tc.sourceDir, tc.data)
-		if err := rs.Populate(fs); err != nil {
-			t.Errorf("rs.Populate(%+v) == %v, want <nil>", fs, err)
-			continue
-		}
-		if diff, equal := messagediff.PrettyDiff(tc.want, rs); !equal {
-			t.Errorf("rs.Populate(%+v) diff:\n%s\n", fs, diff)
-		}
+		t.Run(tc.name, func(t *testing.T) {
+			fs, err := absfstesting.MakeMemMapFs(tc.fs)
+			if err != nil {
+				t.Fatalf("absfstesting.MakeMemMapFs(%v) == %v, %v, want !<nil>, <nil>", tc.fs, fs, err)
+			}
+			rs := NewRootState("/", 0, tc.sourceDir, tc.data)
+			if err := rs.Populate(fs); err != nil {
+				t.Fatalf("rs.Populate(%+v) == %v, want <nil>", fs, err)
+			}
+			if diff, equal := messagediff.PrettyDiff(tc.want, rs); !equal {
+				t.Errorf("rs.Populate(%+v) diff:\n%s\n", fs, diff)
+			}
+		})
 	}
 }
 
 func TestEndToEnd(t *testing.T) {
-	for i, tc := range []struct {
+	for _, tc := range []struct {
+		name      string
 		fsMap     map[string]string
 		sourceDir string
 		data      map[string]interface{}
@@ -222,6 +234,7 @@ func TestEndToEnd(t *testing.T) {
 		wantFsMap map[string]string
 	}{
 		{
+			name: "all",
 			fsMap: map[string]string{
 				"/home/user/.bashrc":                "foo",
 				"/home/user/.chezmoi/dot_bashrc":    "bar",
@@ -249,27 +262,25 @@ func TestEndToEnd(t *testing.T) {
 			},
 		},
 	} {
-		fs, err := absfstesting.MakeMemMapFs(tc.fsMap)
-		if err != nil {
-			t.Errorf("case %d: absfstesting.MakeMemMapFs(%v) == %v, %v, want !<nil>, <nil>", i, tc.fsMap, fs, err)
-			continue
-		}
-		rs := NewRootState(tc.targetDir, tc.umask, tc.sourceDir, tc.data)
-		if err := rs.Populate(fs); err != nil {
-			t.Errorf("rs.Populate(%+v) == %v, want <nil>", fs, err)
-			continue
-		}
-		if err := rs.Apply(fs, NewLoggingActuator(NewFsActuator(fs, tc.targetDir))); err != nil {
-			t.Errorf("case %d: rs.Apply(absfstesting.MakeMemMapFs(%v), _) == %v, want <nil>", i, tc.fsMap, err)
-			continue
-		}
-		gotFsMap, err := absfstesting.MakeMapFs(fs)
-		if err != nil {
-			t.Errorf("case %d: absfstesting.MakeMapFs(%v) == %v, %v, want !<nil>, <nil>", i, fs, gotFsMap, err)
-			continue
-		}
-		if diff, equal := messagediff.PrettyDiff(tc.wantFsMap, gotFsMap); !equal {
-			t.Errorf("case %d:\n%s\n", i, diff)
-		}
+		t.Run(tc.name, func(t *testing.T) {
+			fs, err := absfstesting.MakeMemMapFs(tc.fsMap)
+			if err != nil {
+				t.Fatalf("absfstesting.MakeMemMapFs(%v) == %v, %v, want !<nil>, <nil>", tc.fsMap, fs, err)
+			}
+			rs := NewRootState(tc.targetDir, tc.umask, tc.sourceDir, tc.data)
+			if err := rs.Populate(fs); err != nil {
+				t.Fatalf("rs.Populate(%+v) == %v, want <nil>", fs, err)
+			}
+			if err := rs.Apply(fs, NewLoggingActuator(NewFsActuator(fs, tc.targetDir))); err != nil {
+				t.Fatalf("rs.Apply(absfstesting.MakeMemMapFs(%v), _) == %v, want <nil>", tc.fsMap, err)
+			}
+			gotFsMap, err := absfstesting.MakeMapFs(fs)
+			if err != nil {
+				t.Fatalf("absfstesting.MakeMapFs(%v) == %v, %v, want !<nil>, <nil>", fs, gotFsMap, err)
+			}
+			if diff, equal := messagediff.PrettyDiff(tc.wantFsMap, gotFsMap); !equal {
+				t.Errorf("%s\n", diff)
+			}
+		})
 	}
 }
