083d25d845537dec04d5584398d577b896554a49
Tom Payne <twpayne@gmail.com>
2018-12-02 15:18:45 +0100

    Add --apply, --diff, and --prompt flags to edit command, fixes #28

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -38,6 +38,7 @@ type Config struct {
 	Data             map[string]interface{}
 	funcs            template.FuncMap
 	add              addCommandConfig
+	edit             editCommandConfig
 	keyring          keyringCommandConfig
 }
 
