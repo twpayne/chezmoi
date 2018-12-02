6a33cdc80774fbcafd280b9e1e545699cbee862c
Tom Payne <twpayne@gmail.com>
2018-12-02 16:45:22 +0100

    Add Actuator.Rename

--- lib/chezmoi/actuator.go
+++ lib/chezmoi/actuator.go
@@ -7,6 +7,7 @@ type Actuator interface {
 	Chmod(name string, mode os.FileMode) error
 	Mkdir(name string, perm os.FileMode) error
 	RemoveAll(name string) error
+	Rename(oldpath, newpath string) error
 	WriteFile(filename string, data []byte, perm os.FileMode, currData []byte) error
 	WriteSymlink(oldname, newname string) error
 }
