e375f2552acdeb48680a66dfc1eeec6bbf276996
Tom Payne <twpayne@gmail.com>
2018-12-04 14:07:39 +0100

    Use getEntries in dump command

--- cmd/dump.go
+++ cmd/dump.go
@@ -1,9 +1,6 @@
 package cmd
 
 import (
-	"fmt"
-	"path/filepath"
-
 	"github.com/davecgh/go-spew/spew"
 	"github.com/spf13/cobra"
 	"github.com/twpayne/go-vfs"
@@ -27,19 +24,12 @@ func (c *Config) runDumpCommand(fs vfs.FS, command *cobra.Command, args []string
 	if len(args) == 0 {
 		spew.Dump(targetState)
 	} else {
-		for _, arg := range args {
-			path, err := filepath.Abs(arg)
-			if err != nil {
-				return err
-			}
-			state, err := targetState.Get(path)
-			if err != nil {
-				return err
-			}
-			if state == nil {
-				return fmt.Errorf("%s: not found", arg)
-			}
-			spew.Dump(state)
+		entries, err := c.getEntries(targetState, args)
+		if err != nil {
+			return err
+		}
+		for _, entry := range entries {
+			spew.Dump(entry)
 		}
 	}
 	return nil
