240ac97c038b975734fa8062a4b60a6c29684c4b
Tom Payne <twpayne@gmail.com>
2018-12-10 12:04:07 +0100

    Add source-path command

---
+++ cmd/source_path.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+	vfs "github.com/twpayne/go-vfs"
+)
+
+var sourcePathCommand = &cobra.Command{
+	Use:   "source-path",
+	Args:  cobra.MinimumNArgs(1),
+	Short: "Print the source path of the specified targets",
+	RunE:  makeRunE(config.runSourcePathCommand),
+}
+
+func init() {
+	rootCommand.AddCommand(sourcePathCommand)
+}
+
+func (c *Config) runSourcePathCommand(fs vfs.FS, cmd *cobra.Command, args []string) error {
+	targetState, err := c.getTargetState(fs)
+	if err != nil {
+		return err
+	}
+	entries, err := c.getEntries(targetState, args)
+	if err != nil {
+		return err
+	}
+	for _, entry := range entries {
+		if _, err := fmt.Println(filepath.Join(targetState.SourceDir, entry.SourceName())); err != nil {
+			return err
+		}
+	}
+	return nil
+}
