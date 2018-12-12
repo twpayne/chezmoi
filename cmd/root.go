twpayne/chezmoi

commit 9dc8b103be177b35141c0b6f2011213895b3e2cb

Remove --source-vcs flag

cmd/root.go
@@ -38,9 +38,6 @@ func init() {
 	persistentFlags.StringVarP(&config.SourceDir, "source", "s", filepath.Join(homeDir, ".chezmoi"), "source directory")
 	viper.BindPFlag("source", persistentFlags.Lookup("source"))
 
-	persistentFlags.StringVar(&config.SourceVCSCommand, "source-vcs", "git", "source version control system command")
-	viper.BindPFlag("source-vcs", persistentFlags.Lookup("source-vcs"))
-
 	persistentFlags.StringVarP(&config.TargetDir, "target", "t", homeDir, "target directory")
 	viper.BindPFlag("target", persistentFlags.Lookup("target"))
 

commit 144b1256ef8d9d77c5fa3cf71f45523b0631a2ec

Restore default source VCS command
This was broken by 9dc8b10.

cmd/root.go
@@ -13,7 +13,9 @@ import (
 
 var (
 	configFile string
-	config     Config
+	config     = Config{
+		SourceVCSCommand: "git",
+	}
 )
 
 var rootCommand = &cobra.Command{