7abd4cc859c810e365f3f16ef21adee108f66df0
Tom Payne <twpayne@gmail.com>
2019-01-25 00:55:30 +0100

    Parse permissions in octal

--- cmd/perm_value.go
+++ cmd/perm_value.go
@@ -5,12 +5,12 @@ import (
 	"strconv"
 )
 
-// An permValue is an int that is printed in octal. It implements the
-// pflag.Value interface for use as a command line flag.
+// An permValue is an int that is scanned and printed in octal. It implements
+// the pflag.Value interface for use as a command line flag.
 type permValue int
 
 func (p *permValue) Set(s string) error {
-	v, err := strconv.ParseInt(s, 0, 64)
+	v, err := strconv.ParseInt(s, 8, 64)
 	*p = permValue(v)
 	return err
 }
