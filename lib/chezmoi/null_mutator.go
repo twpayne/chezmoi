035988d53b55b93948cff680e492c25e3ed07317
Tom Payne <twpayne@gmail.com>
2019-02-02 20:19:39 -0800

    Revert "Remove NullMutator"
    This reverts commit 02636ad0491c5513a06e2c68550205c66fddef3f.

---
+++ lib/chezmoi/null_mutator.go
@@ -0,0 +1,43 @@
+package chezmoi
+
+import "os"
+
+type nullMutator struct{}
+
+// NullMutator is an Mutator that does nothing.
+var NullMutator nullMutator
+
+// Chmod implements Mutator.Chmod.
+func (nullMutator) Chmod(string, os.FileMode) error {
+	return nil
+}
+
+// Mkdir implements Mutator.Mkdir.
+func (nullMutator) Mkdir(string, os.FileMode) error {
+	return nil
+}
+
+// RemoveAll implements Mutator.RemoveAll.
+func (nullMutator) RemoveAll(string) error {
+	return nil
+}
+
+// Rename implements Mutator.Rename.
+func (nullMutator) Rename(string, string) error {
+	return nil
+}
+
+// Stat implements Mutator.Stat.
+func (nullMutator) Stat(string) (os.FileInfo, error) {
+	return nil, nil
+}
+
+// WriteFile implements Mutator.WriteFile.
+func (nullMutator) WriteFile(string, []byte, os.FileMode, []byte) error {
+	return nil
+}
+
+// WriteSymlink implements Mutator.WriteSymlink.
+func (nullMutator) WriteSymlink(string, string) error {
+	return nil
+}
