58bf7e92b9e2100c1c45f5ac65901c3d2774e9dd
Tom Payne <twpayne@gmail.com>
2019-04-14 17:17:38 +0200

    Fix calculation of current source dir mode

--- cmd/config.go
+++ cmd/config.go
@@ -157,7 +157,7 @@ func (c *Config) ensureSourceDirectory(fs vfs.FS, mutator chezmoi.Mutator) error
 	info, err := fs.Stat(c.SourceDir)
 	switch {
 	case err == nil && info.IsDir():
-		if info.Mode().Perm()&os.FileMode(c.Umask) != 0700&^os.FileMode(c.Umask) {
+		if info.Mode().Perm()&^os.FileMode(c.Umask) != 0700&^os.FileMode(c.Umask) {
 			if err := mutator.Chmod(c.SourceDir, 0700&^os.FileMode(c.Umask)); err != nil {
 				return err
 			}
