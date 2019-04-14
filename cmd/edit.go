68b2c6a96d4311a460288095b265567b3f7a5e8b
Tom Payne <twpayne@gmail.com>
2019-04-14 20:55:26 +0200

    Make edit work on plaintext, not ciphertext

--- cmd/edit.go
+++ cmd/edit.go
@@ -2,8 +2,11 @@ package cmd
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
 	"path/filepath"
 
+	"github.com/google/renameio"
 	"github.com/spf13/cobra"
 	"github.com/twpayne/chezmoi/lib/chezmoi"
 	vfs "github.com/twpayne/go-vfs"
@@ -32,28 +35,100 @@ func init() {
 	persistentFlags.BoolVarP(&config.edit.prompt, "prompt", "p", false, "prompt before applying (implies --diff)")
 }
 
+type encryptedFile struct {
+	index          int
+	file           *chezmoi.File
+	ciphertextPath string
+	plaintextPath  string
+}
+
 func (c *Config) runEditCmd(fs vfs.FS, args []string) error {
 	if c.edit.prompt {
 		c.edit.diff = true
 	}
+
 	ts, err := c.getTargetState(fs)
 	if err != nil {
 		return err
 	}
+
 	entries, err := c.getEntries(fs, ts, args)
 	if err != nil {
 		return err
 	}
-	argv := []string{}
-	for _, entry := range entries {
-		argv = append(argv, filepath.Join(c.SourceDir, entry.SourceName()))
+
+	// Build a list of source file names to pass to the editor. Check that each
+	// is either a file or a symlink. If the entry is an encrypted file then
+	// remember it.
+	argv := make([]string, len(entries))
+	var encryptedFiles []encryptedFile
+	for i, entry := range entries {
+		argv[i] = filepath.Join(c.SourceDir, entry.SourceName())
+		if file, ok := entry.(*chezmoi.File); ok {
+			if file.Encrypted {
+				ef := encryptedFile{
+					index:          i,
+					file:           file,
+					ciphertextPath: argv[i],
+				}
+				encryptedFiles = append(encryptedFiles, ef)
+			}
+		} else if _, ok := entry.(*chezmoi.Symlink); !ok {
+			return fmt.Errorf("%s: not a file or symlink", args[i])
+		}
 	}
-	if !c.edit.diff && !c.edit.apply {
+
+	// Short path: if no post-edit actions are required then exec the editor
+	// directly.
+	if !c.edit.diff && !c.edit.apply && len(encryptedFiles) == 0 {
 		return c.execEditor(argv...)
 	}
+
+	// If any of the files are encrypted, create a temporary directory to store
+	// the plaintext contents, decrypt each of them, and update argv to point to
+	// the plaintext file.
+	if len(encryptedFiles) != 0 {
+		tempDir, err := ioutil.TempDir("", "chezmoi")
+		if err != nil {
+			return err
+		}
+		defer os.RemoveAll(tempDir)
+		for i := range encryptedFiles {
+			ef := &encryptedFiles[i]
+			plaintext, err := ef.file.Contents()
+			if err != nil {
+				return err
+			}
+			ef.plaintextPath = filepath.Join(tempDir, ef.file.SourceName())
+			if err := os.MkdirAll(filepath.Dir(ef.plaintextPath), 0700); err != nil {
+				return err
+			}
+			if err := renameio.WriteFile(ef.plaintextPath, plaintext, 0600); err != nil {
+				return err
+			}
+			argv[ef.index] = ef.plaintextPath
+		}
+	}
+
 	if err := c.runEditor(argv...); err != nil {
 		return err
 	}
+
+	// Re-encrypt any encypted files.
+	for _, ef := range encryptedFiles {
+		plaintext, err := ioutil.ReadFile(ef.plaintextPath)
+		if err != nil {
+			return err
+		}
+		ciphertext, err := ts.Encrypt(plaintext)
+		if err != nil {
+			return err
+		}
+		if err := renameio.WriteFile(ef.ciphertextPath, ciphertext, 0644); err != nil {
+			return err
+		}
+	}
+
 	readOnlyFS := vfs.NewReadOnlyFS(fs)
 	applyMutator := c.getDefaultMutator(fs)
 	for i, entry := range entries {
