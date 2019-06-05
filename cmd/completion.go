e40b79765827e57be6526bc48b092a1d068434f8
David Gillies <dave.gillies@gmail.com>
2019-06-04 18:13:27 -0700

    Add fish to completion command description

--- cmd/completion.go
+++ cmd/completion.go
@@ -10,7 +10,7 @@ import (
 var completionCmd = &cobra.Command{
 	Use:       "completion shell",
 	Args:      cobra.ExactArgs(1),
-	Short:     "Output shell completion code for the specified shell (bash or zsh)",
+	Short:     "Output shell completion code for the specified shell (bash, fish or zsh)",
 	Long:      mustGetLongHelp("completion"),
 	Example:   getExample("completion"),
 	ValidArgs: []string{"bash", "fish", "zsh"},
