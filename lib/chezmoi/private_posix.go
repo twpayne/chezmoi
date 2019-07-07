fa6b8663d6ed95ddf6bb97675956498a9e65d374
Zach Bean <forty2@gmail.com>
2019-07-06 20:00:08 -0700

    Fix lint -- the posix implementation of IsPrivate can get away with a vfs.Stater but the Windows one can't.

--- lib/chezmoi/private_posix.go
+++ lib/chezmoi/private_posix.go
@@ -4,6 +4,8 @@ package chezmoi
 
 import "os"
 
+// This implementation doesn't use the extra features of PrivacyStater, but the Windows implementation needs them.
+// nolint:interfacer
 func IsPrivate(fs PrivacyStater, file string, umask os.FileMode) bool {
 	info, err := fs.Stat(file)
 	if err != nil {
