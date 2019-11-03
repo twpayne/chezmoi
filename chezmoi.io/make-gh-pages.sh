add120d71323491a133897a5efe7be0c23c150c6
Tom Payne <twpayne@gmail.com>
2019-11-03 17:34:38 +0100

    Add www.chezmoi.io automation

---
+++ chezmoi.io/make-gh-pages.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -eo pipefail
+
+# generate the new website
+rm -rf public/
+hugo
+
+# clone and checkout the gh-pages branch in a temporary directory
+oldwd=$(pwd)
+tmpdir=$(mktemp -d)
+cleanup() {
+    rm -rf ${tmpdir}
+}
+trap cleanup EXIT
+git clone --branch=gh-pages --local .. ${tmpdir}
+
+# copy the new website to the temporary directory
+rm -rf ${tmpdir}/*
+cp -r public/* ${tmpdir}
+
+# prepare the clone
+cd ${tmpdir}
+git checkout CNAME
+git remote set-url origin https://github.com/twpayne/chezmoi.git
+git fetch origin
+git reset origin/gh-pages
+
+# commit the new website
+if ! git diff --quiet ; then
+    git add .
+    git commit --message "Update gh-pages"
+fi
+
+# give the user the opportunity to push the new website 
+echo "run git push to push the new website"
+
+${SHELL}
