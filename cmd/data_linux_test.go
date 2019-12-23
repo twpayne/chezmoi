d318b1468e972f1474c13a50df00ae8f527e9b5f
Tom Payne <twpayne@gmail.com>
2019-12-23 17:45:50 +0100

    Consolidate data source files

--- cmd/data_linux_test.go
+++ cmd/data_linux_test.go
@@ -9,6 +9,24 @@ import (
 	"github.com/twpayne/go-vfs/vfst"
 )
 
+func TestGetKernelInfo(t *testing.T) {
+	root := map[string]interface{}{
+		"/proc/sys/kernel/version":   "#1 SMP Fri Nov 1 14:28:19 UTC 2019",
+		"/proc/sys/kernel/ostype":    "Linux",
+		"/proc/sys/kernel/osrelease": "4.19.81-microsoft-standard",
+	}
+	fs, cleanup, err := vfst.NewTestFS(root)
+	require.NoError(t, err)
+	defer cleanup()
+	info, err := getKernelInfo(fs)
+	assert.NoError(t, err)
+	assert.Equal(t, map[string]string{
+		"version":   "#1 SMP Fri Nov 1 14:28:19 UTC 2019",
+		"ostype":    "Linux",
+		"osrelease": "4.19.81-microsoft-standard",
+	}, info)
+}
+
 func TestGetOSRelease(t *testing.T) {
 	for _, tc := range []struct {
 		name string
--- cmd/osrelease_notlinux.go
+++
@@ -1,9 +0,0 @@
-// +build !linux
-
-package cmd
-
-import vfs "github.com/twpayne/go-vfs"
-
-func getOSRelease(fs vfs.FS) (map[string]string, error) {
-	return nil, nil
-}
