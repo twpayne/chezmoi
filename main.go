ac77728231bafd5e86497e52933b343e82b44014
Tom Payne <twpayne@gmail.com>
2020-01-24 02:30:47 +0100

    Simplify code generation

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
-//go:generate go run ./internal/generate-assets -o cmd/docs.gen.go -package=cmd -tags=!noembeddocs docs/CHANGES.md docs/CONTRIBUTING.md docs/FAQ.md docs/HOWTO.md docs/INSTALL.md docs/QUICKSTART.md docs/REFERENCE.md
-//go:generate go run ./internal/generate-assets -o cmd/templates.gen.go -package=cmd templates/COMMIT_MESSAGE.tmpl
+//go:generate go run ./internal/generate-assets -o cmd/docs.gen.go -tags=!noembeddocs docs/CHANGES.md docs/CONTRIBUTING.md docs/FAQ.md docs/HOWTO.md docs/INSTALL.md docs/QUICKSTART.md docs/REFERENCE.md
+//go:generate go run ./internal/generate-assets -o cmd/templates.gen.go templates/COMMIT_MESSAGE.tmpl
 //go:generate go run ./internal/generate-helps -o cmd/helps.gen.go -i docs/REFERENCE.md
 
 package main
