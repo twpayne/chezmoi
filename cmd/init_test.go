568fb505ce243b31cd2c7c373d7bd170bb9c83aa
Peter Kristensen <peter@ptx.dk>
2020-03-22 13:22:37 +0100

    Improve windows support by removing carriage return from prompts

--- cmd/init_test.go
+++ cmd/init_test.go
@@ -13,6 +13,42 @@ import (
 	"github.com/twpayne/go-vfs/vfst"
 )
 
+func TestPromptStringIgnoreWindowsNewline(t *testing.T) {
+	fs, cleanup, err := vfst.NewTestFS(map[string]interface{}{
+		"/home/user/.local/share/chezmoi/.chezmoi.toml.tmpl": strings.Join([]string{
+			`{{ $env := promptString "environment [work/home]" -}}`,
+			`[data]`,
+			`  environment = "{{ $env }}"`,
+		}, "\n"),
+	})
+	require.NoError(t, err)
+	defer cleanup()
+
+	c := newTestConfig(
+		fs,
+		withStdin(bytes.NewBufferString("home\r\n")),
+	)
+
+	require.NoError(t, c.createConfigFile())
+
+	vfst.RunTests(t, fs, "",
+		vfst.TestPath("/home/user/.config/chezmoi/chezmoi.toml",
+			vfst.TestModeIsRegular,
+			vfst.TestModePerm(0600),
+			vfst.TestContentsString(
+				strings.Join([]string{
+					`[data]`,
+					`  environment = "home"`,
+				}, "\n"),
+			),
+		),
+	)
+
+	assert.Equal(t, map[string]interface{}{
+		"environment": "home",
+	}, c.Data)
+}
+
 func TestCreateConfigFile(t *testing.T) {
 	fs, cleanup, err := vfst.NewTestFS(map[string]interface{}{
 		"/home/user/.local/share/chezmoi/.chezmoi.yaml.tmpl": strings.Join([]string{
