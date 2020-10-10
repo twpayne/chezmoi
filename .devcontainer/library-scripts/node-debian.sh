f5b78e1472f0a011aeeccf928089491e786c9bcd
Tom Payne <twpayne@gmail.com>
2020-10-10 20:15:28 +0200

    Update devcontainer

--- .devcontainer/library-scripts/node-debian.sh
+++ .devcontainer/library-scripts/node-debian.sh
@@ -3,22 +3,37 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
 #-------------------------------------------------------------------------------------------------------------
-
-# Syntax: ./node-debian.sh [directory to install nvm] [node version to install (use "none" to skip)] [non-root user]
+#
+# Docs: https://github.com/microsoft/vscode-dev-containers/blob/master/script-library/docs/node.md
+#
+# Syntax: ./node-debian.sh [directory to install nvm] [node version to install (use "none" to skip)] [non-root user] [Update rc files flag]
 
 export NVM_DIR=${1:-"/usr/local/share/nvm"}
 export NODE_VERSION=${2:-"lts/*"}
-USERNAME=${3:-"vscode"}
+USERNAME=${3:-"automatic"}
+UPDATE_RC=${4:-"true"}
 
 set -e
 
 if [ "$(id -u)" -ne 0 ]; then
-    echo -e 'Script must be run a root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
     exit 1
 fi
 
-# Treat a user name of "none" or non-existant user as root
-if [ "${USERNAME}" = "none" ] || ! id -u ${USERNAME} > /dev/null 2>&1; then
+# Determine the appropriate non-root user
+if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
+    USERNAME=""
+    POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
+    for CURRENT_USER in ${POSSIBLE_USERS[@]}; do
+        if id -u ${CURRENT_USER} > /dev/null 2>&1; then
+            USERNAME=${CURRENT_USER}
+            break
+        fi
+    done
+    if [ "${USERNAME}" = "" ]; then
+        USERNAME=root
+    fi
+elif [ "${USERNAME}" = "none" ] || ! id -u ${USERNAME} > /dev/null 2>&1; then
     USERNAME=root
 fi
 
@@ -56,29 +71,16 @@ if [ -d "${NVM_DIR}" ]; then
     exit 0
 fi
 
-mkdir -p ${NVM_DIR}
-
-NVM_INIT=$(cat <<EOF
-export NVM_DIR="${NVM_DIR}"
-[ -s "\$NVM_DIR/nvm.sh" ] && . "\$NVM_DIR/nvm.sh"
-[ -s "\$NVM_DIR/bash_completion" ] && . "\$NVM_DIR/bash_completion"
-if [ "\$(stat -c '%U' \$NVM_DIR)" != "${USERNAME}" ]; then
-    sudo chown -R ${USERNAME}:root \$NVM_DIR
-fi
-EOF
-)
-
-echo "${NVM_INIT}" | tee -a /root/.bashrc /root/.zshrc >> /etc/skel/.bashrc 
-# Set up non-root user if applicable
-if [ "${USERNAME}" != "root" ]; then   
-    # Add NVM init and add code to update NVM ownership if UID/GID changes
-    echo "${NVM_INIT}" | tee -a /home/${USERNAME}/.bashrc >> /home/${USERNAME}/.zshrc
-    chown ${USERNAME} ${NVM_DIR} /home/${USERNAME}/.bashrc /home/${USERNAME}/.zshrc
-fi
 
 # Run NVM installer as non-root if needed
+mkdir -p ${NVM_DIR}
+chown ${USERNAME} ${NVM_DIR}
 su ${USERNAME} -c "$(cat << EOF
     set -e
+
+    # Do not update profile - we'll do this manually
+    export PROFILE=/dev/null
+
     curl -so- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash 
     source ${NVM_DIR}/nvm.sh
     if [ "${NODE_VERSION}" != "" ]; then
@@ -87,5 +89,31 @@ su ${USERNAME} -c "$(cat << EOF
     nvm clear-cache 
 EOF
 )" 2>&1
+
+if [ "${UPDATE_RC}" = "true" ]; then
+    echo "Updating /etc/bash.bashrc and /etc/zsh/zshrc with NVM scripts..."
+(cat <<EOF
+export NVM_DIR="${NVM_DIR}"
+sudoIf()
+{
+    if [ "\$(id -u)" -ne 0 ]; then
+        sudo "\$@"
+    else
+        "\$@"
+    fi
+}
+if [ "\$(stat -c '%U' \$NVM_DIR)" != "${USERNAME}" ]; then
+    if [ "\$(id -u)" -eq 0 ] || type sudo > /dev/null 2>&1; then
+        echo "Fixing permissions of \"\$NVM_DIR\"..."
+        sudoIf chown -R ${USERNAME}:root \$NVM_DIR
+    else
+        echo "Warning: NVM directory is not owned by ${USERNAME} and sudo is not installed. Unable to correct permissions."
+    fi
+fi
+[ -s "\$NVM_DIR/nvm.sh" ] && . "\$NVM_DIR/nvm.sh"
+[ -s "\$NVM_DIR/bash_completion" ] && . "\$NVM_DIR/bash_completion"
+EOF
+) | tee -a /etc/bash.bashrc >> /etc/zsh/zshrc 
+fi 
 
 echo "Done!"
