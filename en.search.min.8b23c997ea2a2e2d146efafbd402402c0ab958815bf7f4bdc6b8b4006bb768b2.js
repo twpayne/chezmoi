9fe8ca6fc4c977634cce044668c959d9269aca7c
Tom Payne <twpayne@gmail.com>
2020-11-15 03:08:04 +0100

    Update gh-pages

--- en.search.min.8b23c997ea2a2e2d146efafbd402402c0ab958815bf7f4bdc6b8b4006bb768b2.js
+++ en.search.min.8b23c997ea2a2e2d146efafbd402402c0ab958815bf7f4bdc6b8b4006bb768b2.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.354b236cca8e264f784a1420bc0f263feae72b35d363188304d9884afc38a2a9.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.27b47c7c03d53de15980715368d82a90427d9cb929e3bf05e5ccfa82c77dc122.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
