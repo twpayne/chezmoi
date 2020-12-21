1eed6d35ab4ecf67d84037f7cfb1ab966876be1c
Tom Payne <twpayne@gmail.com>
2020-12-21 19:27:54 +0100

    Range check integers parsed from git status

--- internal/git/status.go
+++ internal/git/status.go
@@ -16,9 +16,9 @@ type OrdinaryStatus struct {
 	X    byte
 	Y    byte
 	Sub  string
-	MH   int
-	MI   int
-	MW   int
+	MH   int64
+	MI   int64
+	MW   int64
 	HH   string
 	HI   string
 	Path string
@@ -29,13 +29,13 @@ type RenamedOrCopiedStatus struct {
 	X        byte
 	Y        byte
 	Sub      string
-	MH       int
-	MI       int
-	MW       int
+	MH       int64
+	MI       int64
+	MW       int64
 	HH       string
 	HI       string
 	RC       byte
-	Score    int
+	Score    int64
 	Path     string
 	OrigPath string
 }
@@ -45,10 +45,10 @@ type UnmergedStatus struct {
 	X    byte
 	Y    byte
 	Sub  string
-	M1   int
-	M2   int
-	M3   int
-	MW   int
+	M1   int64
+	M2   int64
+	M3   int64
+	MW   int64
 	H1   string
 	H2   string
 	H3   string
@@ -145,18 +145,25 @@ func ParseStatusPorcelainV2(output []byte) (*Status, error) {
 			if m == nil {
 				return nil, ParseError(text)
 			}
-			var (
-				mH, _ = strconv.ParseInt(text[m[8]:m[9]], 8, 64)
-				mI, _ = strconv.ParseInt(text[m[10]:m[11]], 8, 64)
-				mW, _ = strconv.ParseInt(text[m[12]:m[13]], 8, 64)
-			)
+			mH, err := strconv.ParseInt(text[m[8]:m[9]], 8, 64)
+			if err != nil {
+				return nil, err
+			}
+			mI, err := strconv.ParseInt(text[m[10]:m[11]], 8, 64)
+			if err != nil {
+				return nil, err
+			}
+			mW, err := strconv.ParseInt(text[m[12]:m[13]], 8, 64)
+			if err != nil {
+				return nil, err
+			}
 			os := OrdinaryStatus{
 				X:    text[m[2]],
 				Y:    text[m[4]],
 				Sub:  text[m[6]:m[7]],
-				MH:   int(mH),
-				MI:   int(mI),
-				MW:   int(mW),
+				MH:   mH,
+				MI:   mI,
+				MW:   mW,
 				HH:   text[m[14]:m[15]],
 				HI:   text[m[16]:m[17]],
 				Path: text[m[18]:m[19]],
@@ -167,23 +174,33 @@ func ParseStatusPorcelainV2(output []byte) (*Status, error) {
 			if m == nil {
 				return nil, ParseError(text)
 			}
-			var (
-				mH, _    = strconv.ParseInt(text[m[8]:m[9]], 8, 64)
-				mI, _    = strconv.ParseInt(text[m[10]:m[11]], 8, 64)
-				mW, _    = strconv.ParseInt(text[m[12]:m[13]], 8, 64)
-				score, _ = strconv.ParseInt(text[m[20]:m[21]], 10, 64)
-			)
+			mH, err := strconv.ParseInt(text[m[8]:m[9]], 8, 64)
+			if err != nil {
+				return nil, err
+			}
+			mI, err := strconv.ParseInt(text[m[10]:m[11]], 8, 64)
+			if err != nil {
+				return nil, err
+			}
+			mW, err := strconv.ParseInt(text[m[12]:m[13]], 8, 64)
+			if err != nil {
+				return nil, err
+			}
+			score, err := strconv.ParseInt(text[m[20]:m[21]], 10, 64)
+			if err != nil {
+				return nil, err
+			}
 			rocs := RenamedOrCopiedStatus{
 				X:        text[m[2]],
 				Y:        text[m[4]],
 				Sub:      text[m[6]:m[7]],
-				MH:       int(mH),
-				MI:       int(mI),
-				MW:       int(mW),
+				MH:       mH,
+				MI:       mI,
+				MW:       mW,
 				HH:       text[m[14]:m[15]],
 				HI:       text[m[16]:m[17]],
 				RC:       text[m[18]],
-				Score:    int(score),
+				Score:    score,
 				Path:     text[m[22]:m[23]],
 				OrigPath: text[m[24]:m[25]],
 			}
@@ -193,20 +210,30 @@ func ParseStatusPorcelainV2(output []byte) (*Status, error) {
 			if m == nil {
 				return nil, ParseError(text)
 			}
-			var (
-				m1, _ = strconv.ParseInt(text[m[6]:m[7]], 8, 64)
-				m2, _ = strconv.ParseInt(text[m[8]:m[9]], 8, 64)
-				m3, _ = strconv.ParseInt(text[m[10]:m[11]], 8, 64)
-				mW, _ = strconv.ParseInt(text[m[12]:m[13]], 8, 64)
-			)
+			m1, err := strconv.ParseInt(text[m[6]:m[7]], 8, 64)
+			if err != nil {
+				return nil, err
+			}
+			m2, err := strconv.ParseInt(text[m[8]:m[9]], 8, 64)
+			if err != nil {
+				return nil, err
+			}
+			m3, err := strconv.ParseInt(text[m[10]:m[11]], 8, 64)
+			if err != nil {
+				return nil, err
+			}
+			mW, err := strconv.ParseInt(text[m[12]:m[13]], 8, 64)
+			if err != nil {
+				return nil, err
+			}
 			us := UnmergedStatus{
 				X:   text[m[2]],
 				Y:   text[m[4]],
 				Sub: text[m[6]:m[7]],
-				M1:  int(m1),
-				M2:  int(m2),
-				M3:  int(m3),
-				MW:  int(mW),
+				M1:  m1,
+				M2:  m2,
+				M3:  m3,
+				MW:  mW,
 			}
 			status.Unmerged = append(status.Unmerged, us)
 		case '?':
