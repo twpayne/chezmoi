0a37bea1bdea39cd5c1e27a7282595c3a541695a
Tom Payne <twpayne@gmail.com>
2021-01-22 00:55:49 +0100

    chezmoi2: Restore diff pager

--- chezmoi2/cmd/diffcmd.go
+++ chezmoi2/cmd/diffcmd.go
@@ -1,9 +1,13 @@
 package cmd
 
 import (
+	"bytes"
+	"os/exec"
 	"strings"
+	"unicode"
 
 	"github.com/spf13/cobra"
+	"github.com/twpayne/go-shell"
 
 	"github.com/twpayne/chezmoi/chezmoi2/internal/chezmoi"
 )
@@ -46,5 +50,24 @@ func (c *Config) runDiffCmd(cmd *cobra.Command, args []string) error {
 	}); err != nil {
 		return err
 	}
-	return c.writeOutputString(sb.String())
+
+	if c.Diff.NoPager || c.Diff.Pager == "" {
+		return c.writeOutputString(sb.String())
+	}
+
+	// If the pager command contains any spaces, assume that it is a full
+	// shell command to be executed via the user's shell. Otherwise, execute
+	// it directly.
+	var pagerCmd *exec.Cmd
+	if strings.IndexFunc(c.Diff.Pager, unicode.IsSpace) != -1 {
+		shell, _ := shell.CurrentUserShell()
+		pagerCmd = exec.Command(shell, "-c", c.Diff.Pager)
+	} else {
+		//nolint:gosec
+		pagerCmd = exec.Command(c.Diff.Pager)
+	}
+	pagerCmd.Stdin = bytes.NewReader([]byte(sb.String()))
+	pagerCmd.Stdout = c.stdout
+	pagerCmd.Stderr = c.stderr
+	return pagerCmd.Run()
 }
