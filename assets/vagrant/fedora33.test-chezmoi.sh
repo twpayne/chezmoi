dd116f795ed6bbade15a850b3536dc88a826189c
Tom Payne <twpayne@gmail.com>
2021-09-10 14:46:00 +0200

    Build with Go 1.17.1

--- assets/vagrant/fedora33.test-chezmoi.sh
+++ assets/vagrant/fedora33.test-chezmoi.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-go get golang.org/dl/go1.17
-"$HOME"/go/bin/go1.17 download
-( cd /chezmoi && "$HOME"/go/bin/go1.17 test ./... )
+go get golang.org/dl/go1.17.1
+"$HOME"/go/bin/go1.17.1 download
+( cd /chezmoi && "$HOME"/go/bin/go1.17.1 test ./... )
