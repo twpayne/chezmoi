ea592671272947e511c688f9a22bde97574c502b
Tom Payne <twpayne@gmail.com>
2021-09-17 00:08:31 +0200

    Update Vagrant-based tests

--- assets/vagrant/test.sh
+++ assets/vagrant/test.sh
@@ -3,12 +3,14 @@
 for os in "$@"; do
     if [ -f "${os}.Vagrantfile" ]; then
         export VAGRANT_VAGRANTFILE=assets/vagrant/${os}.Vagrantfile
-        (
-            cd ../.. &&
-            vagrant up &&
-            vagrant ssh -c "sh test-chezmoi.sh" &&
-            vagrant destroy -f
-        )
+        if ( cd ../.. && vagrant up ); then
+            vagrant ssh -c "sh test-chezmoi.sh"
+            vagrantSSHExitCode=$?
+            vagrant destroy -f || exit 1
+            if [ $vagrantSSHExitCode -ne 0 ]; then
+                exit $vagrantSSHExitCode
+            fi
+        fi
     else
         echo "${os}.Vagrantfile not found"
     fi
