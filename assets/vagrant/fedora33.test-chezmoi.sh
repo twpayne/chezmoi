8407cc91d97ca1637dd8ce3aad4bb659e669611d
Tom Payne <twpayne@gmail.com>
2021-09-26 15:22:17 +0000

    chore: Move Linux-based integration tests from Vagrant to Docker

--- assets/vagrant/fedora33.test-chezmoi.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-GO_VERSION=$(grep GO_VERSION: /chezmoi/.github/workflows/main.yml | awk '{ print $2 }')
-go get "golang.org/dl/go${GO_VERSION}"
-"${HOME}/go/bin/go${GO_VERSION}" download
-( cd /chezmoi && "${HOME}/go/bin/go${GO_VERSION}" test ./... )
