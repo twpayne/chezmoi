d90d18fbecfc9f33c4c61ee98eeffa7bd1bf0d84
Tom Payne <twpayne@gmail.com>
2021-09-27 19:49:37 +0200

    chore: Add builtin age encryption test

--- internal/chezmoi/ageencryption_test.go
+++ internal/chezmoi/ageencryption_test.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/twpayne/go-vfs/v4"
 
 	"github.com/twpayne/chezmoi/v2/internal/chezmoitest"
 )
@@ -30,6 +31,31 @@ func TestAgeEncryption(t *testing.T) {
 		Command:   command,
 		Identity:  AbsPath(privateKeyFile),
 		Recipient: publicKey,
+	}
+
+	testEncryptionDecryptToFile(t, ageEncryption)
+	testEncryptionEncryptDecrypt(t, ageEncryption)
+	testEncryptionEncryptFile(t, ageEncryption)
+}
+
+func TestBuiltinAgeEncryption(t *testing.T) {
+	_, err := exec.LookPath("age")
+	if errors.Is(err, exec.ErrNotFound) {
+		t.Skip("age not found in $PATH")
+	}
+	require.NoError(t, err)
+
+	publicKey, privateKeyFile, err := chezmoitest.AgeGenerateKey("")
+	require.NoError(t, err)
+	defer func() {
+		assert.NoError(t, os.RemoveAll(filepath.Dir(privateKeyFile)))
+	}()
+
+	ageEncryption := &AgeEncryption{
+		UseBuiltin: true,
+		BaseSystem: NewRealSystem(vfs.OSFS),
+		Identity:   AbsPath(privateKeyFile),
+		Recipient:  publicKey,
 	}
 
 	testEncryptionDecryptToFile(t, ageEncryption)
