503c3109fdc1b2f565eb3e4ecedbb7c14e5c9c6d
Tom Payne <twpayne@gmail.com>
2021-10-06 21:43:33 +0200

    chore: Remove dependency on age for builtin age tests

--- internal/chezmoi/ageencryption_test.go
+++ internal/chezmoi/ageencryption_test.go
@@ -2,11 +2,13 @@ package chezmoi
 
 import (
 	"errors"
+	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"testing"
 
+	"filippo.io/age"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/twpayne/go-vfs/v4"
@@ -39,26 +41,24 @@ func TestAgeEncryption(t *testing.T) {
 }
 
 func TestBuiltinAgeEncryption(t *testing.T) {
-	_, err := exec.LookPath("age")
-	if errors.Is(err, exec.ErrNotFound) {
-		t.Skip("age not found in $PATH")
-	}
-	require.NoError(t, err)
-
-	publicKey, privateKeyFile, err := chezmoitest.AgeGenerateKey("")
-	require.NoError(t, err)
-	defer func() {
-		assert.NoError(t, os.RemoveAll(filepath.Dir(privateKeyFile)))
-	}()
+	recipientStringer, identityAbsPath := builtinAgeGenerateKey(t)
 
 	ageEncryption := &AgeEncryption{
 		UseBuiltin: true,
 		BaseSystem: NewRealSystem(vfs.OSFS),
-		Identity:   NewAbsPath(privateKeyFile),
-		Recipient:  publicKey,
+		Identity:   identityAbsPath,
+		Recipient:  recipientStringer.String(),
 	}
 
 	testEncryptionDecryptToFile(t, ageEncryption)
 	testEncryptionEncryptDecrypt(t, ageEncryption)
 	testEncryptionEncryptFile(t, ageEncryption)
+}
+
+func builtinAgeGenerateKey(t *testing.T) (fmt.Stringer, AbsPath) {
+	identity, err := age.GenerateX25519Identity()
+	require.NoError(t, err)
+	privateKeyFile := filepath.Join(t.TempDir(), "chezmoi-builtin-age-key.txt")
+	require.NoError(t, os.WriteFile(privateKeyFile, []byte(identity.String()), 0o600))
+	return identity.Recipient(), NewAbsPath(privateKeyFile)
 }
