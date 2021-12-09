66728aa83f923ab49c285c1a36b64d42a020b08c
Tom Payne <twpayne@gmail.com>
2021-12-09 23:19:51 +0100

    feat: Follow symlinks in .chezmoitemplates

--- internal/chezmoi/sourcestate.go
+++ internal/chezmoi/sourcestate.go
@@ -1089,6 +1089,9 @@ func (s *SourceState) addTemplateData(sourceAbsPath AbsPath) error {
 // addTemplatesDir adds all templates in templatesDirAbsPath to s.
 func (s *SourceState) addTemplatesDir(templatesDirAbsPath AbsPath) error {
 	walkFunc := func(templateAbsPath AbsPath, fileInfo fs.FileInfo, err error) error {
+		if err == nil && fileInfo.Mode().Type() == fs.ModeSymlink {
+			fileInfo, err = s.system.Stat(templateAbsPath)
+		}
 		switch {
 		case err != nil:
 			return err
