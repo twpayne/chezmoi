b10e2ddf50abf6a786dc7676ce303068ead3f9d0
Tom Payne <twpayne@gmail.com>
2021-12-14 15:31:06 +0000

    fix: Add missing --value flag to secret keyring set command

--- internal/cmd/secretkeyringcmd.go
+++ internal/cmd/secretkeyringcmd.go
@@ -6,6 +6,16 @@ import (
 )
 
 type secretKeyringCmdConfig struct {
+	get secretKeyringGetCmdConfig
+	set secretKeyringSetCmdConfig
+}
+
+type secretKeyringGetCmdConfig struct {
+	service string
+	user    string
+}
+
+type secretKeyringSetCmdConfig struct {
 	service string
 	user    string
 	value   string
@@ -18,17 +28,16 @@ func (c *Config) newSecretKeyringCmd() *cobra.Command {
 		Short: "Interact with keyring",
 	}
 
-	persistentFlags := keyringCmd.PersistentFlags()
-	persistentFlags.StringVar(&c.secretKeyring.service, "service", "", "service")
-	persistentFlags.StringVar(&c.secretKeyring.user, "user", "", "user")
-	markPersistentFlagsRequired(keyringCmd, "service", "user")
-
 	keyringGetCmd := &cobra.Command{
 		Use:   "get",
 		Args:  cobra.NoArgs,
 		Short: "Get a value from keyring",
 		RunE:  c.runKeyringGetCmdE,
 	}
+	secretKeyringGetPersistentFlags := keyringGetCmd.PersistentFlags()
+	secretKeyringGetPersistentFlags.StringVar(&c.secretKeyring.get.service, "service", "", "service")
+	secretKeyringGetPersistentFlags.StringVar(&c.secretKeyring.get.user, "user", "", "user")
+	markPersistentFlagsRequired(keyringGetCmd, "service", "user")
 	keyringCmd.AddCommand(keyringGetCmd)
 
 	keyringSetCmd := &cobra.Command{
@@ -37,13 +46,18 @@ func (c *Config) newSecretKeyringCmd() *cobra.Command {
 		Short: "Set a value in keyring",
 		RunE:  c.runKeyringSetCmdE,
 	}
+	secretKeyringSetPersistentFlags := keyringSetCmd.PersistentFlags()
+	secretKeyringSetPersistentFlags.StringVar(&c.secretKeyring.set.service, "service", "", "service")
+	secretKeyringSetPersistentFlags.StringVar(&c.secretKeyring.set.user, "user", "", "user")
+	secretKeyringSetPersistentFlags.StringVar(&c.secretKeyring.set.value, "value", "", "value")
+	markPersistentFlagsRequired(keyringSetCmd, "service", "user")
 	keyringCmd.AddCommand(keyringSetCmd)
 
 	return keyringCmd
 }
 
 func (c *Config) runKeyringGetCmdE(cmd *cobra.Command, args []string) error {
-	value, err := keyring.Get(c.secretKeyring.service, c.secretKeyring.user)
+	value, err := keyring.Get(c.secretKeyring.get.service, c.secretKeyring.get.user)
 	if err != nil {
 		return err
 	}
@@ -51,7 +65,7 @@ func (c *Config) runKeyringGetCmdE(cmd *cobra.Command, args []string) error {
 }
 
 func (c *Config) runKeyringSetCmdE(cmd *cobra.Command, args []string) error {
-	value := c.secretKeyring.value
+	value := c.secretKeyring.set.value
 	if value == "" {
 		var err error
 		value, err = c.readPassword("Value: ")
@@ -59,5 +73,5 @@ func (c *Config) runKeyringSetCmdE(cmd *cobra.Command, args []string) error {
 			return err
 		}
 	}
-	return keyring.Set(c.secretKeyring.service, c.secretKeyring.user, value)
+	return keyring.Set(c.secretKeyring.set.service, c.secretKeyring.set.user, value)
 }
