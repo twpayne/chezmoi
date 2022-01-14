785eb6f7c5177b9ac35f663a9eea475420d87fca
Tom Payne <twpayne@gmail.com>
2022-01-14 16:31:27 +0100

    feat: Add chezmoi.io/get install URL

--- internal/cmds/lint-whitespace/main.go
+++ internal/cmds/lint-whitespace/main.go
@@ -20,6 +20,7 @@ var (
 		regexp.MustCompile(`\A\.idea\z`),
 		regexp.MustCompile(`\A\.vagrant\z`),
 		regexp.MustCompile(`\A\.vscode/settings\.json\z`),
+		regexp.MustCompile(`\Aassets/chezmoi\.io/content/get\.ps1\z`),
 		regexp.MustCompile(`\Aassets/chezmoi\.io/public\z`),
 		regexp.MustCompile(`\Aassets/chezmoi\.io/resources\z`),
 		regexp.MustCompile(`\Aassets/chezmoi\.io/themes/book\z`),
