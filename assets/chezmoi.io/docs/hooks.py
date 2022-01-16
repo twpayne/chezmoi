4dd00606838f56728f820ec16fa8942b9c1b6ce9
Tom Payne <twpayne@gmail.com>
2022-01-16 20:12:23 +0000

    chore: Fix website infrastructure

--- assets/chezmoi.io/docs/hooks.py
+++ assets/chezmoi.io/docs/hooks.py
@@ -4,6 +4,9 @@ import shutil
 def on_post_build(config, **kwargs):
     site_dir = config['site_dir']
 
+    # copy GitHub pages config
+    shutil.copy('CNAME', os.path.join(site_dir, 'CNAME'))
+
     # copy installation scripts
     shutil.copy('../scripts/install.sh', os.path.join(site_dir, 'get'))
     shutil.copy('../scripts/install.ps1', os.path.join(site_dir, 'get.ps1'))
