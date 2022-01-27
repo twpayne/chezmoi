bb771a0fc8b035ddf3b6bcea838e7a7e8a02e0ab
Tom Payne <twpayne@gmail.com>
2022-01-28 00:52:02 +0100

    chore: Add systeminfo check to doctor command

--- pkg/cmd/doctorcmd_windows.go
+++ pkg/cmd/doctorcmd_windows.go
@@ -1,6 +1,43 @@
 package cmd
 
-type (
-	umaskCheck struct{ skippedCheck }
-	unameCheck struct{ skippedCheck }
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"os/exec"
+	"strings"
+
+	"github.com/twpayne/chezmoi/v2/pkg/chezmoi"
 )
+
+type (
+	systeminfoCheck struct{}
+	umaskCheck      struct{ skippedCheck }
+	unameCheck      struct{ skippedCheck }
+)
+
+func (systeminfoCheck) Name() string {
+	return "systeminfo"
+}
+
+func (systeminfoCheck) Run(system chezmoi.System, homeDirAbsPath chezmoi.AbsPath) (checkResult, string) {
+	cmd := exec.Command("systeminfo")
+	data, err := system.IdempotentCmdOutput(cmd)
+	if err != nil {
+		return checkResultFailed, err.Error()
+	}
+
+	var osName, osVersion string
+	s := bufio.NewScanner(bytes.NewReader(data))
+	for s.Scan() {
+		switch key, value, found := chezmoi.CutString(s.Text(), ":"); {
+		case !found:
+			// Do nothing.
+		case key == "OS Name":
+			osName = strings.TrimSpace(value)
+		case key == "OS Version":
+			osVersion = strings.TrimSpace(value)
+		}
+	}
+	return checkResultOK, fmt.Sprintf("%s (%s)", osName, osVersion)
+}
