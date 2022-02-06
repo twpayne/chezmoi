de1ceb544aab94e1be8e17154321b6e30451d798
Tom Payne <twpayne@gmail.com>
2022-02-06 00:03:40 +0000

    chore: Generate articles, podcasts, and videos doc from template with data

--- assets/chezmoi.io/docs/hooks.py
+++ assets/chezmoi.io/docs/hooks.py
@@ -1,17 +1,45 @@
 import os
-import shutil
+import subprocess
+
+from mkdocs import utils
+from mkdocs.structure.files import File
+
+non_website_paths = [
+    "hooks.py",
+    "reference/commands/commands.go",
+    "reference/commands/commands_test.go",
+]
+
+templates = [
+    "links/articles-podcasts-and-videos.md",
+]
+
+def on_pre_build(config, **kwargs):
+    docs_dir = config['docs_dir']
+    for src_path in templates:
+        output = docs_dir + "/" + src_path
+        template = output + '.tmpl'
+        data = output + '.yaml'
+        subprocess.run(['go', 'run', '../../internal/cmds/execute-template', '-data', data, '-output', output, template])
+
+def on_files(files, config, **kwargs):
+    # remove non-website files
+    for src_path in non_website_paths:
+        files.remove(files.get_file_from_path(src_path))
+
+    # remove templates and data
+    for src_path in templates:
+        files.remove(files.get_file_from_path(src_path + '.tmpl'))
+        files.remove(files.get_file_from_path(src_path + '.yaml'))
+
+    return files
 
 def on_post_build(config, **kwargs):
     site_dir = config['site_dir']
 
     # copy GitHub pages config
-    shutil.copy('CNAME', os.path.join(site_dir, 'CNAME'))
+    utils.copy_file('CNAME', os.path.join(site_dir, 'CNAME'))
 
     # copy installation scripts
-    shutil.copy('../scripts/install.sh', os.path.join(site_dir, 'get'))
-    shutil.copy('../scripts/install.ps1', os.path.join(site_dir, 'get.ps1'))
-
-    # remove non-website files
-    os.remove(os.path.join(site_dir, 'hooks.py'))
-    os.remove(os.path.join(site_dir, 'reference/commands/commands.go'))
-    os.remove(os.path.join(site_dir, 'reference/commands/commands_test.go'))
+    utils.copy_file('../scripts/install.sh', os.path.join(site_dir, 'get'))
+    utils.copy_file('../scripts/install.ps1', os.path.join(site_dir, 'get.ps1'))
