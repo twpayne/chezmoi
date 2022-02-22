33fa84ac0c81aad516ed64e024c4fb1a5d67a598
Kenny Younger <kyounger@gmail.com>
2022-02-22 12:26:47 -0600

    Revert previous
    This reverts commit 9d7ffd965915583e9ce0d081ad4a7af9a3767a92.

--- assets/scripts/install.sh
+++ assets/scripts/install.sh
@@ -8,12 +8,11 @@
 set -e
 
 BINDIR=${BINDIR:-./bin}
-GITHUB_USER=${GITHUB_USER:-twpayne}
 TAGARG=latest
 LOG_LEVEL=2
 EXECARGS=
 
-GITHUB_DOWNLOAD=https://github.com/${GITHUB_USER}/chezmoi/releases/download
+GITHUB_DOWNLOAD=https://github.com/twpayne/chezmoi/releases/download
 
 tmpdir=$(mktemp -d)
 trap 'rm -rf ${tmpdir}' EXIT
@@ -207,7 +206,7 @@ get_libc() {
 real_tag() {
 	tag=$1
 	log_debug "checking GitHub for tag ${tag}"
-	release_url="https://github.com/${GITHUB_USER}/chezmoi/releases/${tag}"
+	release_url="https://github.com/twpayne/chezmoi/releases/${tag}"
 	json=$(http_get "${release_url}" "Accept: application/json")
 	if [ -z "${json}" ]; then
 		log_err "real_tag error retrieving GitHub release ${tag}"
