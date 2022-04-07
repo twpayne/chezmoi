bf1d1fd013d11cd6f2c49e0678c6419783fe4239
Ruijie Yu <ruijie+git@netyu.xyz>
2022-04-07 00:18:58 -0500

    feat: Make unmanaged command accept destination directory args

--- assets/chezmoi.io/docs/reference/commands/unmanaged.md
+++ assets/chezmoi.io/docs/reference/commands/unmanaged.md
@@ -1,9 +1,13 @@
-# `unmanaged`
+# `unmanaged` [*target*...]
 
-List all unmanaged files in the destination directory.
+List all unmanaged files in *target*s.  When no *target*s are supplied, list all
+unmanaged files in the destination directory.
+
+It is an error to supply *target*s that are not found on the filesystem.
 
 !!! example
 
     ```console
     $ chezmoi unmanaged
+    $ chezmoi unmanaged ~/.config/chezmoi ~/.ssh
     ```
--- pkg/cmd/unmanagedcmd.go
+++ pkg/cmd/unmanagedcmd.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"io/fs"
+	"sort"
 	"strings"
 
 	"github.com/spf13/cobra"
@@ -12,11 +13,11 @@ import (
 
 func (c *Config) newUnmanagedCmd() *cobra.Command {
 	unmanagedCmd := &cobra.Command{
-		Use:     "unmanaged",
+		Use:     "unmanaged [paths]...",
 		Short:   "List the unmanaged files in the destination directory",
 		Long:    mustLongHelp("unmanaged"),
 		Example: example("unmanaged"),
-		Args:    cobra.NoArgs,
+		Args:    cobra.ArbitraryArgs,
 		RunE:    c.makeRunEWithSourceState(c.runUnmanagedCmd),
 	}
 
@@ -24,7 +25,8 @@ func (c *Config) newUnmanagedCmd() *cobra.Command {
 }
 
 func (c *Config) runUnmanagedCmd(cmd *cobra.Command, args []string, sourceState *chezmoi.SourceState) error {
-	builder := strings.Builder{}
+	// the set of discovered, unmanaged items
+	unmanaged := map[string]bool{}
 	walkFunc := func(destAbsPath chezmoi.AbsPath, fileInfo fs.FileInfo, err error) error {
 		if err != nil {
 			return err
@@ -32,20 +34,59 @@ func (c *Config) runUnmanagedCmd(cmd *cobra.Command, args []string, sourceState 
 		if destAbsPath == c.DestDirAbsPath {
 			return nil
 		}
-		targeRelPath := destAbsPath.MustTrimDirPrefix(c.DestDirAbsPath)
-		managed := sourceState.Contains(targeRelPath)
-		ignored := sourceState.Ignore(targeRelPath)
+		targetRelPath, err := destAbsPath.TrimDirPrefix(c.DestDirAbsPath)
+		if err != nil {
+			return err
+		}
+		managed := sourceState.Contains(targetRelPath)
+		ignored := sourceState.Ignore(targetRelPath)
 		if !managed && !ignored {
-			builder.WriteString(targeRelPath.String())
-			builder.WriteByte('\n')
+			unmanaged[targetRelPath.String()] = true
 		}
 		if fileInfo.IsDir() && (!managed || ignored) {
 			return vfs.SkipDir
 		}
 		return nil
 	}
-	if err := chezmoi.Walk(c.destSystem, c.DestDirAbsPath, walkFunc); err != nil {
-		return err
+
+	// Build queued paths. When no arguments, start from root; otherwise start
+	// from arguments.	The paths are deduplicated and sorted.
+	paths := make([]chezmoi.AbsPath, 0, len(args)) // (lsttype, size, capacity)
+	if len(args) == 0 {
+		paths = append(paths, c.DestDirAbsPath)
+	} else {
+		qPaths := make(map[chezmoi.AbsPath]bool, len(args)) // (map, capacity)
+		for _, arg := range args {
+			p, err := chezmoi.NormalizePath(arg)
+			if err != nil {
+				return err
+			}
+			qPaths[p] = true
+		}
+		for path := range qPaths {
+			paths = append(paths, path)
+		}
+		sort.Slice(paths,
+			func(i, j int) bool { return paths[i].Less(paths[j]) })
+	}
+
+	for _, path := range paths {
+		if err := chezmoi.Walk(c.destSystem, path, walkFunc); err != nil {
+			return err
+		}
+	}
+
+	// collect the keys and sort
+	builder := strings.Builder{}
+	unmPaths := make([]string, 0, len(unmanaged))
+	for path := range unmanaged {
+		unmPaths = append(unmPaths, path)
+	}
+	sort.Strings(unmPaths)
+
+	for _, path := range unmPaths {
+		builder.WriteString(path)
+		builder.WriteByte('\n')
 	}
 	return c.writeOutputString(builder.String())
 }
