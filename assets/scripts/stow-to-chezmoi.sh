58105c84259688d7b70310098408cfa9cee00b49
Lue <lue@lued.me>
2022-06-03 23:05:50 -0400

    fix: Improve quality of POSIX shell scripts

--- assets/scripts/stow-to-chezmoi.sh
+++ assets/scripts/stow-to-chezmoi.sh
@@ -1,29 +1,31 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 set -e
 
-BASEDIR="${1:-$HOME}"
+BASEDIR="${1:-${HOME}}"
 STOWDIR="${2:-dotfiles}"
 
 BASEDIR="$(
-	unset CDPATH
-	cd "${BASEDIR}" >/dev/null 2>&1
-	pwd
+	unset -v CDPATH
+	cd -- "${BASEDIR}" >/dev/null 2>&1
+	pwd && printf .
 )"
+BASEDIR="${BASEDIR%??}"
 
 # if we have greadlink, use that
-READLINK="$(which greadlink 2>/dev/null || which readlink)"
+READLINK="$(command -v greadlink 2>/dev/null || command -v readlink)"
 
 removelink() {
-	[ -L "$1" ] && (
-		LINK_DEST="$($READLINK -f "$1")"
-		rm "$1"
-		echo -ne "${LINK_DEST} ==> $1\t"
-		if cp -R "${LINK_DEST}" "$1"; then
-			echo "Done"
+	[ -h "${1}" ] && (
+		LINK_DEST="$("${READLINK}" -f -- "${1}" && printf .)"
+		LINK_DEST="${LINK_DEST%??}"
+		rm -- "${1}"
+		printf '%s ==> %s\t' "${LINK_DEST}" "${1}" >&2
+		if cp -r -- "${LINK_DEST}" "${1}"; then
+			printf 'Done\n' >&2
 		else
-			echo "FAILED"
-			return 1
+			printf 'FAILED\n' >&2
+			exit 1
 		fi
 	)
 }
@@ -31,32 +33,37 @@ removelink() {
 work_file="$(mktemp)"
 act_file="$(mktemp)"
 
-trap 'rm -f ${work_file} ${act_file}' EXIT
+# attempt to clean up temporary files on exit
+trap 'rm -f -- "${work_file}" "${act_file}"' EXIT
+trap 'exit' INT TERM
 
-find "${BASEDIR}" -not -path "${BASEDIR}/${STOWDIR}*" -type l >"${work_file}" || echo "Find skipped some files"
+find "${BASEDIR}" \! -path '*
+*' \! -path "${BASEDIR}/${STOWDIR}*" -type l >"${work_file}" || \
+	printf "Find skipped some files\n" >&2
 
 while read -r f; do
-	target="$($READLINK -f "${f}" || echo '')"
-	if [[ "${target}" == "${BASEDIR}/${STOWDIR}/"* ]]; then
-		echo "Add $f"
-		echo "${f}" >>"${act_file}"
-	fi
+	target="$("${READLINK}" -f -- "${f}" || :)"
+	case "${target}" in
+		"${BASEDIR}/${STOWDIR}/"*)
+			printf 'Add %s\n' "${f}" >&2
+			printf '%s\n' "${f}" >>"${act_file}"
+			;;
+	esac
 done <"${work_file}"
 
-read -p "Migrate the above to chezmoi? y/N" -r migrate
+printf 'Migrate the above to chezmoi? y/N ' >&2
+read -r migrate
 case "${migrate}" in
-[Yy]*)
-	echo "Migrating..."
-	;;
-*) exit 1 ;;
+	[Yy]*) printf 'Migrating...\n' >&2 ;;
+	*) exit 1 ;;
 esac
 
-mkdir -p "${BASEDIR}/.local/share"
+mkdir -p -- "${BASEDIR}/.local/share"
 
 while read -r f; do
 	if removelink "${f}"; then
-		chezmoi --source "${BASEDIR}/.local/share/chezmoi" --destination "${BASEDIR}" add "${f}"
+		chezmoi --source "${BASEDIR}/.local/share/chezmoi" --destination "${BASEDIR}" add -- "${f}"
 	else
-		echo "Unable to move: $f"
+		printf 'Unable to move: %s\n' "${f}" >&2
 	fi
 done <"${act_file}"
