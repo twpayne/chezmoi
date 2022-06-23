99cff664320825c066b9e15a1b7dfb41ad15004b
Tom Payne <twpayne@gmail.com>
2022-06-23 01:06:51 +0000

    chore: Update dependencies

--- completions/chezmoi.fish
+++ completions/chezmoi.fish
@@ -18,7 +18,8 @@ function __chezmoi_perform_completion
     __chezmoi_debug "args: $args"
     __chezmoi_debug "last arg: $lastArg"
 
-    set -l requestComp "$args[1] __complete $args[2..-1] $lastArg"
+    # Disable ActiveHelp which is not supported for fish shell
+    set -l requestComp "CHEZMOI_ACTIVE_HELP=0 $args[1] __complete $args[2..-1] $lastArg"
 
     __chezmoi_debug "Calling $requestComp"
     set -l results (eval $requestComp 2> /dev/null)
