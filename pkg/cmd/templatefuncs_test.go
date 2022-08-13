02b89540d75f8e9f122bcda66235321a687140be
Tom Payne <twpayne@gmail.com>
2022-08-13 09:08:01 +0200

    feat: Add fromIni template function

--- pkg/cmd/templatefuncs_test.go
+++ pkg/cmd/templatefuncs_test.go
@@ -2,11 +2,50 @@ package cmd
 
 import (
 	"errors"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"gopkg.in/ini.v1"
+
+	"github.com/twpayne/chezmoi/v2/pkg/chezmoitest"
 )
+
+func TestFromIniTemplateFunc(t *testing.T) {
+	for i, tc := range []struct {
+		text     string
+		expected map[string]any
+	}{
+		{
+			text: chezmoitest.JoinLines(
+				`key = value`,
+			),
+			expected: map[string]any{
+				ini.DefaultSection: map[string]any{
+					"key": "value",
+				},
+			},
+		},
+		{
+			text: chezmoitest.JoinLines(
+				`[section]`,
+				`sectionKey = sectionValue`,
+			),
+			expected: map[string]any{
+				ini.DefaultSection: map[string]any{},
+				"section": map[string]any{
+					"sectionKey": "sectionValue",
+				},
+			},
+		},
+	} {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			c := &Config{}
+			assert.Equal(t, tc.expected, c.fromIniTemplateFunc(tc.text))
+		})
+	}
+}
 
 func TestQuoteListTemplateFunc(t *testing.T) {
 	c, err := newConfig()
