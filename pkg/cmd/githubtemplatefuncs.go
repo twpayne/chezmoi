ffbb36718ad32fdc631aa08d61f1fce409f675a7
Tom Payne <twpayne@gmail.com>
2022-08-18 14:22:56 +0200

    feat: Add gitHubLatestTag template function

--- pkg/cmd/githubtemplatefuncs.go
+++ pkg/cmd/githubtemplatefuncs.go
@@ -11,8 +11,11 @@ import (
 )
 
 type gitHubData struct {
+	client             *github.Client
+	clientErr          error
 	keysCache          map[string][]*github.Key
 	latestReleaseCache map[string]map[string]*github.RepositoryRelease
+	latestTagCache     map[string]map[string]*github.RepositoryTag
 }
 
 func (c *Config) gitHubKeysTemplateFunc(user string) []*github.Key {
@@ -23,11 +26,10 @@ func (c *Config) gitHubKeysTemplateFunc(user string) []*github.Key {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	httpClient, err := c.getHTTPClient()
+	gitHubClient, err := c.getGitHubClient(ctx)
 	if err != nil {
 		panic(err)
 	}
-	gitHubClient := chezmoi.NewGitHubClient(ctx, httpClient)
 
 	var allKeys []*github.Key
 	opts := &github.ListOptions{
@@ -53,25 +55,24 @@ func (c *Config) gitHubKeysTemplateFunc(user string) []*github.Key {
 }
 
 func (c *Config) gitHubLatestReleaseTemplateFunc(userRepo string) *github.RepositoryRelease {
-	user, repo, ok := strings.Cut(userRepo, "/")
-	if !ok {
-		panic(fmt.Errorf("%s: not a user/repo", userRepo))
+	owner, repo, err := gitHubSplitOwnerRepo(userRepo)
+	if err != nil {
+		panic(err)
 	}
 
-	if release := c.gitHub.latestReleaseCache[user][repo]; release != nil {
+	if release := c.gitHub.latestReleaseCache[owner][repo]; release != nil {
 		return release
 	}
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	httpClient, err := c.getHTTPClient()
+	gitHubClient, err := c.getGitHubClient(ctx)
 	if err != nil {
 		panic(err)
 	}
-	gitHubClient := chezmoi.NewGitHubClient(ctx, httpClient)
 
-	release, _, err := gitHubClient.Repositories.GetLatestRelease(ctx, user, repo)
+	release, _, err := gitHubClient.Repositories.GetLatestRelease(ctx, owner, repo)
 	if err != nil {
 		panic(err)
 	}
@@ -79,10 +80,73 @@ func (c *Config) gitHubLatestReleaseTemplateFunc(userRepo string) *github.Reposi
 	if c.gitHub.latestReleaseCache == nil {
 		c.gitHub.latestReleaseCache = make(map[string]map[string]*github.RepositoryRelease)
 	}
-	if c.gitHub.latestReleaseCache[user] == nil {
-		c.gitHub.latestReleaseCache[user] = make(map[string]*github.RepositoryRelease)
+	if c.gitHub.latestReleaseCache[owner] == nil {
+		c.gitHub.latestReleaseCache[owner] = make(map[string]*github.RepositoryRelease)
 	}
-	c.gitHub.latestReleaseCache[user][repo] = release
+	c.gitHub.latestReleaseCache[owner][repo] = release
 
 	return release
+}
+
+func (c *Config) gitHubLatestTagTemplateFunc(userRepo string) *github.RepositoryTag {
+	owner, repo, err := gitHubSplitOwnerRepo(userRepo)
+	if err != nil {
+		panic(err)
+	}
+
+	if tag, ok := c.gitHub.latestTagCache[owner][repo]; ok {
+		return tag
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	gitHubClient, err := c.getGitHubClient(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	tags, _, err := gitHubClient.Repositories.ListTags(ctx, owner, repo, &github.ListOptions{
+		PerPage: 1,
+	})
+	if err != nil {
+		panic(err)
+	}
+	var firstTag *github.RepositoryTag
+	if len(tags) > 0 {
+		firstTag = tags[0]
+	}
+
+	if c.gitHub.latestTagCache == nil {
+		c.gitHub.latestTagCache = make(map[string]map[string]*github.RepositoryTag)
+	}
+	if c.gitHub.latestTagCache[owner] == nil {
+		c.gitHub.latestTagCache[owner] = make(map[string]*github.RepositoryTag)
+	}
+	c.gitHub.latestTagCache[owner][repo] = firstTag
+
+	return firstTag
+}
+
+func (c *Config) getGitHubClient(ctx context.Context) (*github.Client, error) {
+	if c.gitHub.client != nil || c.gitHub.clientErr != nil {
+		return c.gitHub.client, c.gitHub.clientErr
+	}
+
+	httpClient, err := c.getHTTPClient()
+	if err != nil {
+		c.gitHub.clientErr = err
+		return nil, err
+	}
+
+	c.gitHub.client = chezmoi.NewGitHubClient(ctx, httpClient)
+	return c.gitHub.client, nil
+}
+
+func gitHubSplitOwnerRepo(userRepo string) (string, string, error) {
+	user, repo, ok := strings.Cut(userRepo, "/")
+	if !ok {
+		return "", "", fmt.Errorf("%s: not a user/repo", userRepo)
+	}
+	return user, repo, nil
 }
