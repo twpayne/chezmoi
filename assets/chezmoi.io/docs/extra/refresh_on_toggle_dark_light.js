3b6edafefc478944fd1f0534996a055889381a7e
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2022-08-30 14:08:29 +0900

    docs: Fix a coloring issue that lines and texts in Mermaid diagrams are hardly visible in dark mode
    Thanks to descriptions and instructions in mkdocs-mermaid2-plugin's README:
    https://github.com/fralau/mkdocs-mermaid2-plugin/blob/master/README.md#material-theme-switching-the-mermaid-diagram-on-the-fly-between-light-and-dark-mode

---
+++ assets/chezmoi.io/docs/extra/refresh_on_toggle_dark_light.js
@@ -0,0 +1,10 @@
+var paletteSwitcher1 = document.getElementById("__palette_1");
+var paletteSwitcher2 = document.getElementById("__palette_2");
+
+paletteSwitcher1.addEventListener("change", function () {
+  location.reload();
+});
+
+paletteSwitcher2.addEventListener("change", function () {
+  location.reload();
+});
