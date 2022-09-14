cf512599d7045bcdb92a9b4c932ed757462ed5e4
Tom Payne <twpayne@gmail.com>
2022-09-14 23:56:18 +0200

    feat: Use bubbletea for user input

---
+++ pkg/chezmoibubbles/boolinputmodel_test.go
@@ -0,0 +1,70 @@
+package chezmoibubbles
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBoolInputModel(t *testing.T) {
+	for _, tc := range []struct {
+		name             string
+		defaultValue     *bool
+		input            string
+		expectedCanceled bool
+		expectedValue    bool
+	}{
+		{
+			name:          "empty_with_default",
+			defaultValue:  newBool(true),
+			input:         "\r",
+			expectedValue: true,
+		},
+		{
+			name:             "cancel_ctrlc",
+			input:            "\x03",
+			expectedCanceled: true,
+		},
+		{
+			name:             "cancel_esc",
+			input:            "\x1b",
+			expectedCanceled: true,
+		},
+		{
+			name:          "true",
+			input:         "t",
+			expectedValue: true,
+		},
+		{
+			name:          "false",
+			input:         "f",
+			expectedValue: false,
+		},
+		{
+			name:          "yes",
+			input:         "y",
+			expectedValue: true,
+		},
+		{
+			name:          "no",
+			input:         "n",
+			expectedValue: false,
+		},
+		{
+			name:          "one",
+			input:         "1",
+			expectedValue: true,
+		},
+		{
+			name:          "zero",
+			input:         "0",
+			expectedValue: false,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			actualModel := testRunModelWithInput(t, NewBoolInputModel("prompt", tc.defaultValue), tc.input)
+			assert.Equal(t, tc.expectedCanceled, actualModel.Canceled())
+			assert.Equal(t, tc.expectedValue, actualModel.Value())
+		})
+	}
+}
---
+++ pkg/chezmoibubbles/intinputmodel_test.go
@@ -0,0 +1,60 @@
+package chezmoibubbles
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIntInputModel(t *testing.T) {
+	for _, tc := range []struct {
+		name             string
+		defaultValue     *int64
+		input            string
+		expectedCanceled bool
+		expectedValue    int64
+	}{
+		{
+			name:          "empty_with_default",
+			defaultValue:  newInt64(1),
+			input:         "\r",
+			expectedValue: 1,
+		},
+		{
+			name:             "cancel_ctrlc",
+			input:            "\x03",
+			expectedCanceled: true,
+		},
+		{
+			name:             "cancel_esc",
+			input:            "\x1b",
+			expectedCanceled: true,
+		},
+		{
+			name:          "one_enter",
+			input:         "1\r",
+			expectedValue: 1,
+		},
+		{
+			name:          "minus_one_enter",
+			input:         "-1\r",
+			expectedValue: -1,
+		},
+		{
+			name:          "minus_enter",
+			input:         "-\r",
+			expectedValue: 0,
+		},
+		{
+			name:          "one_invalid_enter",
+			input:         "1a\r",
+			expectedValue: 1,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			actualModel := testRunModelWithInput(t, NewIntInputModel("prompt", tc.defaultValue), tc.input)
+			assert.Equal(t, tc.expectedCanceled, actualModel.Canceled())
+			assert.Equal(t, tc.expectedValue, actualModel.Value())
+		})
+	}
+}
