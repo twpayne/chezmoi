c26c10d55c61167689b317ecf5d611a9a5fac579
Tom Payne <twpayne@gmail.com>
2022-09-23 14:27:08 +0200

    chore: Use EnableVirtualTerminalProcessing from muesli/termenv

--- pkg/cmd/util_windows.go
+++ pkg/cmd/util_windows.go
@@ -1,12 +1,7 @@
 package cmd
 
 import (
-	"fmt"
-	"io"
 	"io/fs"
-	"os"
-
-	"golang.org/x/sys/windows"
 
 	"github.com/twpayne/chezmoi/v2/pkg/chezmoi"
 )
@@ -31,37 +26,6 @@ var defaultInterpreters = map[string]*chezmoi.Interpreter{
 	"rb": {
 		Command: "ruby",
 	},
-}
-
-// enableVirtualTerminalProcessing enables virtual terminal processing on
-// Windows systems. See
-// https://docs.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences.
-// It returns a function that restores the console to the previous state.
-func enableVirtualTerminalProcessing(w io.Writer) (func() error, error) {
-	file, ok := w.(*os.File)
-	if !ok {
-		return nil, nil
-	}
-
-	var dwMode uint32
-	if err := windows.GetConsoleMode(windows.Handle(file.Fd()), &dwMode); err != nil {
-		// Ignore error in the case that fd is not a terminal.
-		return nil, nil
-	}
-
-	if dwMode&windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING == windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING {
-		// If virtual terminal processing is already enabled, then there is
-		// nothing to do.
-		return nil, nil
-	}
-
-	if err := windows.SetConsoleMode(windows.Handle(file.Fd()), dwMode|windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING); err != nil {
-		return nil, fmt.Errorf("windows.SetConsoleMode: %w", err)
-	}
-
-	return func() error {
-		return windows.SetConsoleMode(windows.Handle(file.Fd()), dwMode)
-	}, nil
 }
 
 func fileInfoUID(fs.FileInfo) int {
