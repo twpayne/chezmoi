c15d5b33ddd3d2f64ef8b1fd18cf2ee9a8c87467
Austin Ziegler <austin@zieglers.ca>
2022-10-18 22:49:12 -0400

    fix: Extend template directives functionality
    Fixes #2468
    
    Template directives have been added, but some places were missed in the
    initial implementation and review (oops).
    
    - pkg/cmd/templatefuncs.go (`includeTemplateTemplateFunc`)
    - pkg/chezmoi/sourcestate.go (`addTemplatesDir`)
    - pkg/cmd/config.go (`createConfigFile`)
    - pkg/cmd/config.go (`gitautoCommit`)
    
    The last is not necessary (since the template is only included as an
    asset and cannot be replaced), so it can be reversed without issue.

--- pkg/cmd/config.go
+++ pkg/cmd/config.go
@@ -700,7 +700,7 @@ func (c *Config) createConfigFile(filename chezmoi.RelPath, data []byte) ([]byte
 	}
 	chezmoi.RecursiveMerge(funcMap, initTemplateFuncs)
 
-	t, err := template.New(filename.String()).Funcs(funcMap).Parse(string(data))
+	t, err := chezmoi.NewConfiguredTemplate(filename.String(), data, funcMap)
 	if err != nil {
 		return nil, err
 	}
@@ -1285,7 +1285,7 @@ func (c *Config) gitAutoCommit(status *git.Status) error {
 	funcMap["targetRelPath"] = func(source string) string {
 		return chezmoi.NewSourceRelPath(source).TargetRelPath(c.encryption.EncryptedSuffix()).String()
 	}
-	commitMessageTmpl, err := template.New("commit_message").Funcs(funcMap).Parse(string(commitMessageTemplate))
+	commitMessageTmpl, err := chezmoi.NewConfiguredTemplate("commit_message", commitMessageTemplate, funcMap)
 	if err != nil {
 		return err
 	}
