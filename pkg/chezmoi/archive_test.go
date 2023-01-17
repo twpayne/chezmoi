e74a926a464961f66cdb3476eb1f2ef79f56f5e9
Luke Karrys <luke@lukekarrys.com>
2023-01-17 15:18:15 -0700

    fix: Walk flattened external archives

--- pkg/chezmoi/archive_test.go
+++ pkg/chezmoi/archive_test.go
@@ -12,50 +12,73 @@ import (
 )
 
 func TestWalkArchive(t *testing.T) {
+	nestedRoot := map[string]any{
+		"dir1": map[string]any{
+			"subdir1": map[string]any{
+				"file1": "",
+				"file2": "",
+			},
+			"subdir2": map[string]any{
+				"file1": "",
+				"file2": "",
+			},
+		},
+		"dir2": map[string]any{
+			"subdir1": map[string]any{
+				"file1": "",
+				"file2": "",
+			},
+			"subdir2": map[string]any{
+				"file1": "",
+				"file2": "",
+			},
+		},
+		"file1":    "",
+		"file2":    "",
+		"symlink1": &archivetest.Symlink{Target: "file1"},
+		"symlink2": &archivetest.Symlink{Target: "file2"},
+	}
+	flatRoot := map[string]any{
+		"dir1/subdir1/file1": "",
+		"dir1/subdir1/file2": "",
+		"dir1/subdir2/file1": "",
+		"dir1/subdir2/file2": "",
+		"dir2/subdir1/file1": "",
+		"dir2/subdir1/file2": "",
+		"dir2/subdir2/file1": "",
+		"dir2/subdir2/file2": "",
+		"file1":              "",
+		"file2":              "",
+		"symlink1":           &archivetest.Symlink{Target: "file1"},
+		"symlink2":           &archivetest.Symlink{Target: "file2"},
+	}
 	for _, tc := range []struct {
 		name          string
+		root          map[string]any
 		dataFunc      func(map[string]any) ([]byte, error)
 		archiveFormat ArchiveFormat
 	}{
 		{
 			name:          "tar",
+			root:          nestedRoot,
 			dataFunc:      archivetest.NewTar,
 			archiveFormat: ArchiveFormatTar,
 		},
 		{
 			name:          "zip",
+			root:          nestedRoot,
 			dataFunc:      archivetest.NewZip,
 			archiveFormat: ArchiveFormatZip,
 		},
+		{
+			name:          "tar-flat",
+			root:          flatRoot,
+			dataFunc:      archivetest.NewTar,
+			archiveFormat: ArchiveFormatTar,
+		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
-			root := map[string]any{
-				"dir1": map[string]any{
-					"subdir1": map[string]any{
-						"file1": "",
-						"file2": "",
-					},
-					"subdir2": map[string]any{
-						"file1": "",
-						"file2": "",
-					},
-				},
-				"dir2": map[string]any{
-					"subdir1": map[string]any{
-						"file1": "",
-						"file2": "",
-					},
-					"subdir2": map[string]any{
-						"file1": "",
-						"file2": "",
-					},
-				},
-				"file1":    "",
-				"file2":    "",
-				"symlink1": &archivetest.Symlink{Target: "file1"},
-				"symlink2": &archivetest.Symlink{Target: "file2"},
-			}
-			data, err := tc.dataFunc(root)
+			data, err := tc.dataFunc(tc.root)
 			require.NoError(t, err)
 
 			expectedNames := []string{
