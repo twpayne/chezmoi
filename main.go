5e6043c5f70bf790380683347b7e29c72341b337
Tom Payne <twpayne@gmail.com>
2023-03-12 00:18:59 +0100

    chore: Enforce Go 1.19 minimum version

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-//go:build go1.18
+//go:build go1.19
 
 //go:generate go run . completion bash -o completions/chezmoi-completion.bash
 //go:generate go run . completion fish -o completions/chezmoi.fish
