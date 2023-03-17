9d8a47246dd8a39ff6b4a0ecf6b010167568880a
Tom Payne <twpayne@gmail.com>
2023-03-17 21:01:33 +0100

    chore: Tidy up ejson default key dir

--- pkg/cmd/ejsontemplatefuncs.go
+++ pkg/cmd/ejsontemplatefuncs.go
@@ -2,14 +2,8 @@ package cmd
 
 import (
 	"encoding/json"
-	"os"
 
 	"github.com/Shopify/ejson"
-)
-
-const (
-	ejsonDefaultKeyDir = "/opt/ejson/keys"
-	ejsonEnvKeyDir     = "EJSON_KEYDIR"
 )
 
 type ejsonConfig struct {
@@ -27,21 +21,7 @@ func (c *Config) ejsonDecryptWithKeyTemplateFunc(filePath, key string) any {
 		c.Ejson.cache = make(map[string]any)
 	}
 
-	/* We accept here that an empty string is considered as if
-	   the value was not provided; in reality, someone could
-	   provide an empty value, but ejson would then look into
-	   the root of the filesystem; this means that this is not
-	   a real limitation, since people could simply set '/'
-	   if wanting to use the filesystem root (or equivalent) */
-	keyDir := c.Ejson.KeyDir
-	if keyDir == "" {
-		keyDir = os.Getenv(ejsonEnvKeyDir)
-	}
-	if keyDir == "" {
-		keyDir = ejsonDefaultKeyDir
-	}
-
-	decrypted, err := ejson.DecryptFile(filePath, keyDir, key)
+	decrypted, err := ejson.DecryptFile(filePath, c.Ejson.KeyDir, key)
 	if err != nil {
 		panic(err)
 	}
