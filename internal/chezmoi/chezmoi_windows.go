658baa83c03f5fa02a73fdf783603bfe31e707c4
Arran Ubels <arran4@gmail.com>
2023-08-05 16:43:35 +1000

    fix: Provide some logic for `isExecutable` under windows for determining if it's an executable
    BREAKING CHANGE: `isExecutable` no longer always returns false and tries to determine execution status by file extension

--- internal/chezmoi/chezmoi_windows.go
+++ internal/chezmoi/chezmoi_windows.go
@@ -2,13 +2,37 @@ package chezmoi
 
 import (
 	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 const nativeLineEnding = "\r\n"
 
-// isExecutable returns false on Windows.
+var pathExt []string = nil
+
+// getPathExt a singleton that obtains the PathExt environment variable and splits it up using the OS `ListSeparator`
+func getPathExt() []string {
+	if pathExt == nil {
+		pathExt = strings.Split(os.Getenv("PathExt"), string(filepath.ListSeparator))
+	}
+	return pathExt
+}
+
+// isExecutable checks if the file has an extension listed in the `PathExt` variable as per:
+// https://www.nextofwindows.com/what-is-pathext-environment-variable-in-windows then checks to see if it's regular file
 func isExecutable(fileInfo fs.FileInfo) bool {
-	return false
+	foundPathExt := false
+	cmdExt := filepath.Ext(fileInfo.Name())
+	if cmdExt != "" {
+		for _, ext := range getPathExt() {
+			if strings.EqualFold(cmdExt, ext) {
+				foundPathExt = true
+				break
+			}
+		}
+	}
+	return foundPathExt && fileInfo.Mode().IsRegular()
 }
 
 // isPrivate returns false on Windows.
