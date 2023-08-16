ddd9ab67f813d9e3ebd6c86f36796f1450b2f919
Braden Hilton <hiltonbraden@gmail.com>
2023-08-16 23:23:23 +0100

    chore: Miscellaneous website improvements

--- assets/chezmoi.io/docs/hooks.py
+++ assets/chezmoi.io/docs/hooks.py
@@ -1,9 +1,7 @@
-import os
-import os.path
 import subprocess
 
+from pathlib import PurePosixPath, Path
 from mkdocs import utils
-from mkdocs.structure.files import File
 
 non_website_paths = [
     "docs.go",
@@ -22,17 +20,19 @@ templates = [
     "reference/release-history.md",
 ]
 
+
 def on_pre_build(config, **kwargs):
-    docs_dir = config['docs_dir']
+    docs_dir = PurePosixPath(config['docs_dir'])
     for src_path in templates:
-        output_path = docs_dir + "/" + src_path
-        template_path = output_path + '.tmpl'
-        data_path = output_path + '.yaml'
+        output_path = docs_dir.joinpath(src_path)
+        template_path = output_path.parent / (output_path.name + '.tmpl')
+        data_path = output_path.parent / (output_path.name + '.yaml')
         args = ['go', 'run', '../../internal/cmds/execute-template']
-        if os.path.exists(data_path):
-            args += ['-data', data_path]
-        args += ['-output', output_path, template_path]
+        if Path(data_path).exists():
+            args.extend(['-data', data_path])
+        args.extend(['-output', output_path, template_path])
         subprocess.run(args)
+
 
 def on_files(files, config, **kwargs):
     # remove non-website files
@@ -48,16 +48,17 @@ def on_files(files, config, **kwargs):
 
     return files
 
+
 def on_post_build(config, **kwargs):
     site_dir = config['site_dir']
 
     # copy GitHub pages config
-    utils.copy_file('CNAME', os.path.join(site_dir, 'CNAME'))
+    utils.copy_file('CNAME', Path(site_dir, 'CNAME'))
 
     # copy installation scripts
-    utils.copy_file('../scripts/install.sh', os.path.join(site_dir, 'get'))
-    utils.copy_file('../scripts/install-local-bin.sh', os.path.join(site_dir, 'getlb'))
-    utils.copy_file('../scripts/install.ps1', os.path.join(site_dir, 'get.ps1'))
+    utils.copy_file('../scripts/install.sh', Path(site_dir, 'get'))
+    utils.copy_file('../scripts/install-local-bin.sh', Path(site_dir, 'getlb'))
+    utils.copy_file('../scripts/install.ps1', Path(site_dir, 'get.ps1'))
 
     # copy cosign.pub
-    utils.copy_file('../cosign/cosign.pub', os.path.join(site_dir, 'cosign.pub'))
+    utils.copy_file('../cosign/cosign.pub', Path(site_dir, 'cosign.pub'))
