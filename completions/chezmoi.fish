8e301ef5d0cf31ddc532b47cfdd09faee1cc57ea
Tom Payne <twpayne@gmail.com>
2023-11-09 01:39:32 +0100

    chore: Update dependencies

--- completions/chezmoi.fish
+++ completions/chezmoi.fish
@@ -79,7 +79,7 @@ function __chezmoi_clear_perform_completion_once_result
     __chezmoi_debug ""
     __chezmoi_debug "========= clearing previously set __chezmoi_perform_completion_once_result variable =========="
     set --erase __chezmoi_perform_completion_once_result
-    __chezmoi_debug "Succesfully erased the variable __chezmoi_perform_completion_once_result"
+    __chezmoi_debug "Successfully erased the variable __chezmoi_perform_completion_once_result"
 end
 
 function __chezmoi_requires_order_preservation
