991ed6352f1553704cd20bfc14b948133ba42f26
Tom Payne <twpayne@gmail.com>
2024-01-24 02:04:43 +0100

    chore: Improve argument names

--- internal/chezmoi/encryption.go
+++ internal/chezmoi/encryption.go
@@ -3,8 +3,8 @@ package chezmoi
 // An Encryption encrypts and decrypts files and data.
 type Encryption interface {
 	Decrypt(ciphertext []byte) ([]byte, error)
-	DecryptToFile(plaintextFilename AbsPath, ciphertext []byte) error
+	DecryptToFile(plaintextAbsPath AbsPath, ciphertext []byte) error
 	Encrypt(plaintext []byte) ([]byte, error)
-	EncryptFile(plaintextFilename AbsPath) ([]byte, error)
+	EncryptFile(plaintextAbsPath AbsPath) ([]byte, error)
 	EncryptedSuffix() string
 }
