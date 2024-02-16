d9938b1657c2ad4c34099d7daecf2e17cb86e4eb
Tom Payne <twpayne@gmail.com>
2024-02-16 19:52:40 +0100

    chore: Work around ruamel.yaml errors when formatting YAML
    Co-authored-by: Braden Hilton <hiltonbraden@gmail.com>

--- assets/scripts/format-yaml.py
+++ assets/scripts/format-yaml.py
@@ -10,10 +10,17 @@ from ruamel.yaml import YAML
 
 def main() -> int:
     yaml = YAML()
+    # ruamel.yaml.YAML will by default use the native line ending, which leads
+    # to differences between Windows and UNIX. Force the output to use UNIX line
+    # endings.
+    yaml.line_break = '\n'
+    # ruamel.yaml.YAML will by default break long lines and introduce trailing
+    # whitespace errors. Disable this behavior by setting a long line width.
+    yaml.width = 1024
     for filename in sys.argv[1:]:
         with Path(filename).open('r') as file:
             data = yaml.load(file)
-        with Path(filename).open('w') as file:
+        with Path(filename).open('w', newline='\n') as file:
             yaml.dump(data, file)
     return 0
 
