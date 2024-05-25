a48a4595564890c634f229c7e592df4bb770870e
Tom Payne <twpayne@gmail.com>
2024-05-25 16:03:21 +0200

    chore: Exclude COMMIT from whitespace checks

--- internal/cmds/lint-whitespace/main.go
+++ internal/cmds/lint-whitespace/main.go
@@ -20,6 +20,7 @@ var (
 		regexp.MustCompile(`\A\.vagrant\z`),
 		regexp.MustCompile(`\A\.venv\z`),
 		regexp.MustCompile(`\A\.vscode\z`),
+		regexp.MustCompile(`\ACOMMIT\z`),
 		regexp.MustCompile(`\Aassets/chezmoi\.io/site\z`),
 		regexp.MustCompile(`\Aassets/scripts/install\.ps1\z`),
 		regexp.MustCompile(`\Acompletions/chezmoi\.ps1\z`),
