f3e9d6cbb0585b5a39bb32c7f23e5d01ba3b4549
Ruslan Sayfutdinov <ruslan@sayfutdinov.com>
2024-11-07 17:46:36 +0000

    chore: Fix typo in entrypoint.sh

--- assets/docker/entrypoint.sh
+++ assets/docker/entrypoint.sh
@@ -7,10 +7,10 @@ git config --global --add safe.directory /chezmoi
 GO=${GO:-go}
 
 if [ -d "/go-cache" ]; then
-	echo "Set GOCACHE to /go-cache"
 	export GOCACHE="/go-cache/cache"
-	echo "Set GOMODCACHE to /go-cache/modcache"
+	echo "Set GOCACHE to ${GOCACHE}"
 	export GOMODCACHE="/go-cache/modcache"
+	echo "Set GOMODCACHE to ${GOMODCACHE}"
 fi
 
 cd /chezmoi
