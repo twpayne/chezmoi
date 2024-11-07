536a6f955cb6495db79bdbc2fab233af3d4208cc
Ruslan Sayfutdinov <ruslan@sayfutdinov.com>
2024-11-07 13:37:39 +0000

    chore: Enable Go cache for Docker builds

--- assets/docker/entrypoint.sh
+++ assets/docker/entrypoint.sh
@@ -6,6 +6,13 @@ git config --global --add safe.directory /chezmoi
 
 GO=${GO:-go}
 
+if [ -d "/go-cache" ]; then
+	echo "Set GOCACHE to /go-cache"
+	export GOCACHE="/go-cache/cache"
+	echo "Set GOMODCACHE to /go-cache/modcache"
+	export GOMODCACHE="/go-cache/modcache"
+fi
+
 cd /chezmoi
 ${GO} run . doctor || true
 ${GO} test ./...
--- assets/docker/test.sh
+++ assets/docker/test.sh
@@ -2,7 +2,15 @@
 
 set -eufo pipefail
 
-cd ../..
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+REPO_ROOT=$( cd "${SCRIPT_DIR}/../.." &> /dev/null && pwd )
+cd "${REPO_ROOT}"
+
+if [ "$#" -eq 0 ]; then
+	echo "Usage: $0 distribution1 [distribution2 ... distributionN]"
+	exit 1
+fi
+
 for distribution in "$@"; do
 	echo "${distribution}"
 	dockerfile="assets/docker/${distribution}.Dockerfile"
@@ -11,12 +19,20 @@ for distribution in "$@"; do
 		exit 1
 	fi
 	image="$(docker build . -f "assets/docker/${distribution}.Dockerfile" -q)"
-	docker run \
-		--env "CHEZMOI_GITHUB_ACCESS_TOKEN=${CHEZMOI_GITHUB_ACCESS_TOKEN-}" \
-		--env "CHEZMOI_GITHUB_TOKEN=${CHEZMOI_GITHUB_TOKEN-}" \
-		--env "GITHUB_ACCESS_TOKEN=${GITHUB_ACCESS_TOKEN-}" \
-		--env "GITHUB_TOKEN=${GITHUB_TOKEN-}" \
-		--rm \
-		--volume "${PWD}:/chezmoi" \
-		"${image}"
+	docker_command=(
+		docker run
+		--env "CHEZMOI_GITHUB_ACCESS_TOKEN=${CHEZMOI_GITHUB_ACCESS_TOKEN-}"
+		--env "CHEZMOI_GITHUB_TOKEN=${CHEZMOI_GITHUB_TOKEN-}"
+		--env "GITHUB_ACCESS_TOKEN=${GITHUB_ACCESS_TOKEN-}"
+		--env "GITHUB_TOKEN=${GITHUB_TOKEN-}"
+		--rm
+		--volume "${PWD}:/chezmoi"
+	)
+	if [ -n "${DOCKER_GOCACHE-}" ]; then
+		mkdir -p "${DOCKER_GOCACHE}"
+		docker_command+=(--volume "${DOCKER_GOCACHE}:/go-cache")
+	fi
+	docker_command+=("${image}")
+	# Run docker
+	"${docker_command[@]}"
 done
