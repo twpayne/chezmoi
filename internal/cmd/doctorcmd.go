1b2b79aaffaaae4255e64fdac2e3e500c581c912
Tom Payne <twpayne@gmail.com>
2024-12-23 17:54:11 +0000

    chore: Update dependencies

--- internal/cmd/doctorcmd.go
+++ internal/cmd/doctorcmd.go
@@ -20,7 +20,7 @@ import (
 	"time"
 
 	"github.com/coreos/go-semver/semver"
-	"github.com/google/go-github/v67/github"
+	"github.com/google/go-github/v68/github"
 	"github.com/spf13/cobra"
 	"github.com/twpayne/go-shell"
 	"github.com/twpayne/go-xdg/v6"
