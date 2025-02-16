16effdeab64e1216f47d1afdab899050fc9aac7d
Austin Ziegler <austin@zieglers.ca>
2025-02-16 12:30:43 -0500

    chore: Upgrade Mermaid version
    Force `mermaid2` to the latest version (11.4.1).
    
    This resulted in a minor bug on rendering where the palette numbering
    was apparently changed from 1 (light) and 2 (dark) to 0 (light) and
    1 (dark). Updated the `refresh_on_toggle_dark_light.js` script to work
    with either scenario. `document.getElementById("__palette_0")` will
    return `null` if using 1 indexing, so we swap the dark palette switcher
    with the light palette switcher and set the dark palette switcher to
    `__palette_2`.

--- assets/chezmoi.io/docs/extra/refresh_on_toggle_dark_light.js
+++ assets/chezmoi.io/docs/extra/refresh_on_toggle_dark_light.js
@@ -1,10 +1,14 @@
-var paletteSwitcher1 = document.getElementById("__palette_1");
-var paletteSwitcher2 = document.getElementById("__palette_2");
+// The light palette is always the first palette. In some cases, it is numbered
+// as __palette_0 and in others it is numbered as __palette_1. The dark palette
+// is numbered as __palette_1 or __palette_2, depending on the index used for
+// the light palette.
+var paletteSwitcherLight = document.getElementById("__palette_0");
+var paletteSwitcherDark = document.getElementById("__palette_1");
 
-paletteSwitcher1.addEventListener("change", function () {
-  location.reload();
-});
+if (!paletteSwitcherLight) {
+  paletteSwitcherLight = paletteSwitcherDark;
+  paletteSwitcherDark = document.getElementById("__palette_2");
+}
 
-paletteSwitcher2.addEventListener("change", function () {
-  location.reload();
-});
+paletteSwitcherLight.addEventListener("change", () => location.reload());
+paletteSwitcherDark.addEventListener("change", () => location.reload());
