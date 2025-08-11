7cb9502729555a5123d53530a07055d55413235b
Tom Payne <twpayne@gmail.com>
2025-08-11 15:24:35 +0000

    chore: Remove unsupported openbsd/riscv64 target

--- assets/scripts/install.sh
+++ assets/scripts/install.sh
@@ -174,7 +174,6 @@ check_goos_goarch() {
 	openbsd/arm) return 0 ;;
 	openbsd/arm64) return 0 ;;
 	openbsd/ppc64) return 0 ;;
-	openbsd/riscv64) return 0 ;;
 	windows/386) return 0 ;;
 	windows/amd64) return 0 ;;
 	*)
