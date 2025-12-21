7d0fd1cea0d70c29ec7166407695e631aa04c7c1
Tom Payne <twpayne@gmail.com>
2025-12-21 21:46:06 +0000

    chore: Snapshot

--- internal/cmd/config_test.go
+++ internal/cmd/config_test.go
@@ -88,7 +88,8 @@ func TestConfigFileFormatRoundTrip(t *testing.T) {
 				},
 				ScriptEnv: map[string]string{},
 				Template: templateConfig{
-					Options: []string{},
+					FunctionsStr: defaultSentinel,
+					Options:      []string{},
 				},
 				TextConv:      []*textConvElement{},
 				UseBuiltinAge: autoBool{value: false},
