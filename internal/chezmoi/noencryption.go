bcd2fa79d5b8efd4c94cd332aa2ae745c23ad8dd
Tom Payne <twpayne@gmail.com>
2026-01-22 18:28:41 +0000

    chore: Improve error message when no encryption is configured

--- internal/chezmoi/noencryption.go
+++ internal/chezmoi/noencryption.go
@@ -2,22 +2,22 @@ package chezmoi
 
 import "errors"
 
-var errNoEncryption = errors.New("no encryption")
+var errEncryptionNotConfigured = errors.New("encryption not configured")
 
 // NoEncryption returns an error when any method is called.
 type NoEncryption struct{}
 
 // Decrypt implements Encryption.Decrypt.
-func (NoEncryption) Decrypt([]byte) ([]byte, error) { return nil, errNoEncryption }
+func (NoEncryption) Decrypt([]byte) ([]byte, error) { return nil, errEncryptionNotConfigured }
 
 // DecryptToFile implements Encryption.DecryptToFile.
-func (NoEncryption) DecryptToFile(AbsPath, []byte) error { return errNoEncryption }
+func (NoEncryption) DecryptToFile(AbsPath, []byte) error { return errEncryptionNotConfigured }
 
 // Encrypt implements Encryption.Encrypt.
-func (NoEncryption) Encrypt([]byte) ([]byte, error) { return nil, errNoEncryption }
+func (NoEncryption) Encrypt([]byte) ([]byte, error) { return nil, errEncryptionNotConfigured }
 
 // EncryptFile implements Encryption.EncryptFile.
-func (NoEncryption) EncryptFile(AbsPath) ([]byte, error) { return nil, errNoEncryption }
+func (NoEncryption) EncryptFile(AbsPath) ([]byte, error) { return nil, errEncryptionNotConfigured }
 
 // EncryptedSuffix implements Encryption.EncryptedSuffix.
 func (NoEncryption) EncryptedSuffix() string { return "" }
